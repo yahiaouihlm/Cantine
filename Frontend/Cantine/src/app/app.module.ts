2823b3627c941599d1e20cc3032f2f0aaa19e7a1
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-13 12:21:42 +0200

    make init  angular project  with  basic responsive nav bar

--- Frontend/Cantine/src/app/app.module.ts
+++ Frontend/Cantine/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreCantineModule} from "./core-cantine/core-cantine.module";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreCantineModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/Cantine/src/app/core-cantine/core-cantine.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: 'cantine', component: HomeComponent}
+];  
+@NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule,
+   
+    RouterModule.forChild(routes),
+  ]
+})
+export class CoreCantineModule { }
