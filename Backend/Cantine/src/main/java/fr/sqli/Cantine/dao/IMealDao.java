cfafff5b85bdbe807337b7d8362d675c26a74ee8
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-14 16:06:20 +0200

    add Some  CONSTRAINT

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IMealDao.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IMealDao.java
@@ -6,8 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-@Repository
-public interface IMealDao  extends JpaRepository<MealEntity, Integer>  {
+import java.util.Optional;
 
-     Boolean existsByLabelAndAndCategoryAndDescription(String label,  String category, String description);
+@Repository
+public interface IMealDao extends JpaRepository<MealEntity, Integer> {
+  /* TODO:  CHANGE THE METHODE */
+    @Query("SELECT meal FROM MealEntity meal  " +
+            "WHERE (LOWER(TRIM(meal.label)) = ?1   OR     TRIM(LOWER(meal.label))=?1  )AND " +
+            "(LOWER(TRIM(meal.category)) = ?2      OR    TRIM(LOWER(meal.category))=?2  )AND " +
+            "LOWER(TRIM(meal.description)) = ?3   OR    TRIM(LOWER(meal.description))=?3 "
+    )
+     List<MealEntity> existsBy(String label, String category, String description);
 }
