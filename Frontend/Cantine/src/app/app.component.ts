8453f8e9814b8528145e673847077b8f4767b299
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-15 15:11:43 +0200

    make the configuration  updates

--- Frontend/Cantine/src/app/app.component.ts
+++ Frontend/Cantine/src/app/app.component.ts
@@ -1,33 +1,13 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./styles/app.component.scss']
+  template: `
+   <router-outlet></router-outlet>
+   `,
+  styleUrls: []
 })
 export class AppComponent {
-  isconnected = true;
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-  constructor (private observer: BreakpointObserver, private cd : ChangeDetectorRef) {}
-  title = 'Cantine';
-
-  ngAfterViewInit() {
-    if (this.isconnected) {
-
-      this.observer.observe(['(max-width: 900px)']).subscribe((res) => {
-        if (res.matches) {
-          this.sidenav.mode = 'over';
-          this.sidenav.close();
-        } else {
-          this.sidenav.mode = 'side';
-          this.sidenav.open();
-        }
-      });
-
-      this.cd.detectChanges();
-
-    }
-  }
+  
 }
--- Frontend/Cantine/src/app/app.module.ts
+++ Frontend/Cantine/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {CoreCantineModule} from "./core-cantine/core-cantine.module";
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
+import { CoreCantineModule } from './core-cantine/core-cantine.module';
 import { RouterModule } from '@angular/router';
-import { MatBadgeModule } from '@angular/material/badge';
-
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,11 +15,6 @@ import { MatBadgeModule } from '@angular/material/badge';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreCantineModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatToolbarModule,
-    MatListModule,
-    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
