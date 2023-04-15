a29ff8d82f216979d901d2b15c062bd2928886bc
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-15 15:58:16 +0200

    add  home componenet

--- Frontend/Cantine/src/app/app-routing.module.ts
+++ Frontend/Cantine/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PageNotFoundComponent} from "./globalComponents/page-not-found/page-not-found.component";
 
 const routes: Routes = [
  {path:'cantine', loadChildren:() => import('./core-cantine/core-cantine.module').then(m => m.CoreCantineModule)},
- {path:'', redirectTo:'cantine/home', pathMatch:'full'}
+ {path:'', redirectTo:'cantine/home', pathMatch:'full'},
+    {path:'**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
   imports: [
- 
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
