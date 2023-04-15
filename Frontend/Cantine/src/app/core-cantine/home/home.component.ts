ee5b2a6f7c02807950fde8998493d70527e3f86f
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-15 16:06:18 +0200

    add assets  to  git repo

--- Frontend/Cantine/src/app/core-cantine/home/home.component.ts
+++ Frontend/Cantine/src/app/core-cantine/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../../assets/home.component.scss']
+  styleUrls: ['../../../assets/styles/home.component.scss']
 })
 export class HomeComponent {
       
