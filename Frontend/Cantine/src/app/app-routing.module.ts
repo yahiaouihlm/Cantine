4ea3db6ac546f90cf2840e0431b09c279550149d
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-16 22:09:37 +0200

    add  new route

--- Frontend/Cantine/src/app/app-routing.module.ts
+++ Frontend/Cantine/src/app/app-routing.module.ts
@@ -3,8 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import {PageNotFoundComponent} from "./globalComponents/page-not-found/page-not-found.component";
 
 const routes: Routes = [
+
  {path:'cantine', loadChildren:() => import('./core-cantine/core-cantine.module').then(m => m.CoreCantineModule)},
- {path:'', redirectTo:'cantine/home', pathMatch:'full'},
+    {path:'', redirectTo:'cantine/home', pathMatch:'full'},
     {path:'**', component: PageNotFoundComponent}
 ];
 
