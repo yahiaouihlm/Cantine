56e909a4e14807ee10dce0520d444c5c729c362a
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-17 11:30:11 +0200

    add  global  footer

---
+++ Frontend/Cantine/src/app/globalComponents/main-footer/main-footer.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-footer',
+  templateUrl: './main-footer.component.html',
+  styles: [
+    `
+       footer{
+         font-size: 1rem;
+    }
+    .footer-container{
+      width: 100%;
+
+
+    }
+      
+      `
+  ]
+})
+export class MainFooterComponent {
+
+}
