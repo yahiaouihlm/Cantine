7a14968fa6f9f8f2c705078fbde4ad5f97033a25
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-18 17:10:19 +0200

    add MenuDao

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IMenuDao.java
@@ -0,0 +1,10 @@
+package fr.sqli.Cantine.dao;
+
+
+import fr.sqli.Cantine.entity.MenuEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMenuDao extends JpaRepository<MenuEntity, Integer> {
+}
