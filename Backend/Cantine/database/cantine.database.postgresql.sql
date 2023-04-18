0425b7e5fa3a8363e40405127c11aec811e816b7
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-18 16:57:42 +0200

    add menu table  and association (menu_has_plat)  table

--- Backend/Cantine/database/cantine.database.postgresql.sql
+++ Backend/Cantine/database/cantine.database.postgresql.sql
@@ -6,7 +6,7 @@
 CREATE TABLE IF NOT EXISTS image(
     idimage SERIAL PRIMARY KEY,
     imageName VARCHAR(400) NOT NULL
-)
+);
 
 
 
@@ -28,10 +28,31 @@ CREATE TABLE IF NOT EXISTS meal(
     CHECK (price >= 0),
     unique (label, description, category),
     FOREIGN KEY (image_idimage) REFERENCES image(idimage) ON DELETE NO ACTION ON UPDATE NO ACTION
-    )
+    );
 
 
+CREATE TABLE IF NOT EXISTS menu (
+    id SERIAL PRIMARY KEY,
+    label  VARCHAR(100) NOT NULL,
+    description  TEXT NOT NULL,
+    status INT  NOT  NULL,
+    price  DECIMAL(5,2) NOT NULL,
+    image_idimage  INT NOT NULL,
+    creatoin_date  DATE NOT NULL,
+    quantite INT NOT NULL ,
+    CHECK (status IN (0,1)),
+    CHECK (quantity >= 0),
+    CHECK (price >= 0),
+    unique (label, description),
+    FOREIGN KEY (image_idimage) REFERENCES image (idimage) ON DELETE NO ACTION ON UPDATE NO ACTION
+    );
 
-
+CREATE TABLE IF NOT EXISTS menu_has_meal (
+     menu_idMenu INT NOT NULL,
+     meal_idmeal INT NOT NULL,
+     PRIMARY KEY (menu_idMenu, plat_idplat),
+    FOREIGN KEY (menu_idMenu) REFERENCES menu (id)ON DELETE CASCADE,
+    FOREIGN KEY (meal_idmeal) REFERENCES  meal (id) ON DELETE RESTRICT
+    );
 
 
