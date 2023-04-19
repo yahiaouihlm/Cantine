5e4338af728d3d2f0dc303344884957b283013a3
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-19 14:51:09 +0200

    add new query  to  check if Existing  meal

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IMealDao.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IMealDao.java
@@ -1,6 +1,7 @@
 package fr.sqli.Cantine.dao;
 
 import fr.sqli.Cantine.entity.MealEntity;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,12 @@ public interface IMealDao extends JpaRepository<MealEntity, Integer> {
   /* TODO:  CHANGE THE METHODE */
 
    @Query(value = "SELECT meal FROM MealEntity meal" +
-           " WHERE LOWER(TRIM(meal.label)) = ?1 AND LOWER(TRIM(meal.category))= ?2 AND LOWER(TRIM(meal.description))= ?3")
+           " WHERE (" +
+           "LOWER(TRIM(meal.label)) = ?1 AND LOWER(TRIM(meal.category))= ?2 AND LOWER(TRIM(meal.description))= ?3" +
+            " OR " +
+           "UPPER(TRIM(meal.label)) = ?1 AND UPPER(TRIM(meal.category))= ?2 AND UPPER(TRIM(meal.description))= ?3" +
+            " OR " +
+              "TRIM(meal.label) = ?1 AND TRIM(meal.category)= ?2 AND TRIM(meal.description)= ?3" +
+           ")")
    Optional  <MealEntity>   findByLabelAndAndCategoryAndDescriptionIgnoreCase(String label, String category, String description);
 }
