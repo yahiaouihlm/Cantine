4b6737aa5a8e2412191f8009d62627375fae010a
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-20 17:04:08 +0200

    add containerTest for  invalid Label

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/AddMealTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/AddMealTest.java
@@ -12,11 +12,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -36,53 +40,49 @@ public class AddMealTest   extends  AbstractMealTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private MealDtoIn mealDtoIn;
+    private MultiValueMap<String, String> formData;
+
+    private MockMultipartFile  imageData;
+
 
     @BeforeEach
     public  void  initMeaDtoIn () throws IOException {
-
-
-       // this.mealDtoIn.setImage(createMultipartFileFromFile(file));
+        this.formData = new LinkedMultiValueMap<>();
+        this.formData.add("label", "MealTest");
+        this.formData.add("price", "1.5");
+        this.formData.add("category", "MealTest category");
+        this.formData.add("description", "MealTest description");
+        this.formData.add("status", "1");
+        this.formData.add("quantity", "10");
+        this.imageData = new MockMultipartFile(
+                "image",                         // nom du champ de fichier
+                "ImageMealForTest.jpg",          // nom du fichier
+                "image/jpg",                    // type MIME
+                new FileInputStream("images/meals/ImageMealForTest.jpg"));
 
     }
 
 
     @Test
     void  AddMealWithNullLabel () throws Exception {
-        // given : mealDtoIn with null label
-        this.mealDtoIn.setLabel(null);
-
-        /// prepare request
-        var  requestBody = new ObjectMapper().writeValueAsString(this.mealDtoIn);
+        // given :  remove label from formData
+        this.formData.remove("label" );
 
        // when : call addMeal
-        var  result  =  this.mockMvc.perform(MockMvcRequestBuilders.post(ADD_MEAL_URL)
-                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
-                .content(requestBody));
+          var result  =  this.mockMvc.perform( MockMvcRequestBuilders.multipart(ADD_MEAL_URL)
+                          .file(this.imageData)
+                    .params(this.formData)
+                  .contentType(MediaType.MULTIPART_FORM_DATA_VALUE));
 
 
+
+        // then :
        result.andExpect(MockMvcResultMatchers.status().isBadRequest())
-               .andExpect(MockMvcResultMatchers.content().string(INVALID_LABEL));
+               .andExpect(MockMvcResultMatchers.content().json(super.exceptionMessage(INVALID_LABEL)) )    ;
 
     }
 
 
-
-
-    public MultipartFile createMultipartFileFromFile(File file) throws IOException {
-        String contentType = "image/jpeg"; // ou récupérez le type de contenu à partir du fichier si vous le connaissez
-        String filename = file.getName();
-        byte[] content = readFileToByteArray(file);
-        return new MockMultipartFile(filename, filename, contentType, content);
-    }
-
-    private byte[] readFileToByteArray(File file) throws IOException {
-        try (InputStream inputStream = new FileInputStream(file)) {
-            return inputStream.readAllBytes();
-        }
-
-
-    }
 
 
 
