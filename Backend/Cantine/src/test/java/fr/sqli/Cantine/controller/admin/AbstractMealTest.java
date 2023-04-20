2d8cadb957279f3eccf4628a639be89767f8d7a0
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-20 17:02:56 +0200

    add exceptionMessage methode with convert the result to  jSON FORMAT

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/AbstractMealTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/AbstractMealTest.java
@@ -1,13 +1,17 @@
 package fr.sqli.Cantine.controller.admin;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public class AbstractMealTest {
-
+    private final  String exceptionMessage = "exceptionMessage";
     protected  final  String BASE_MEAL_URL  =  "/cantine/api/admin/meals";
     protected final  String GET_ALL_MEALS_URL  =  BASE_MEAL_URL  +  "/getAll";
 
     protected   final   String ADD_MEAL_URL  =  BASE_MEAL_URL  +  "/add";
+
+
 
     static   final PostgreSQLContainer    postgreSQLContainer ;
     static {
@@ -18,4 +22,16 @@ public class AbstractMealTest {
         postgreSQLContainer.start();
     }
 
+    /**
+     *  return   String  witch contains exception message combined with right  key= "exceptionMessage" as json format
+     * @param exceptionMessage message to be combined with key "exceptionMessage"
+     * @return String witch contains exception message combined with right  key= "exceptionMessage" as json format
+     * @throws JSONException if the key is not valid.
+     */
+    protected  String  exceptionMessage(String exceptionMessage) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put(this.exceptionMessage, exceptionMessage);
+        return jsonObject.toString();
+
+    }
 }
