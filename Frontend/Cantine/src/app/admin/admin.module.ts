c34a7470c1d02993e99dc9a0fdbf6db88ab3261e
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-21 15:42:00 +0200

    make  router  to  admin module

--- Frontend/Cantine/src/app/admin/admin.module.ts
+++ Frontend/Cantine/src/app/admin/admin.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainAdminComponent } from './main-admin/main-admin.component';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {MatBadgeModule} from "@angular/material/badge";
+import {RouterModule, Routes} from "@angular/router";
+import {CoreCantineModule} from "../core-cantine/core-cantine.module";
 
+
+const routes: Routes = [
+  {path: '', component: MainAdminComponent,
+    children: [
+
+    ]
+  },
+
+  {path:'', redirectTo:'cantine/home', pathMatch:'full'},
+];
 
 
 @NgModule({
@@ -9,7 +26,15 @@ import { MainAdminComponent } from './main-admin/main-admin.component';
     MainAdminComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatBadgeModule,
+    CoreCantineModule,
   ]
 })
 export class AdminModule { }
--- Frontend/Cantine/src/app/admin/main-admin/main-admin.component.ts
+++ Frontend/Cantine/src/app/admin/main-admin/main-admin.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   ]
 })
 export class MainAdminComponent {
-
+  isconnected = false;
 }
