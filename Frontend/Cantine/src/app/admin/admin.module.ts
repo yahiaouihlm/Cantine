58a9e1bc488593c270e0e55a3a4bc6d21926c978
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-21 15:32:13 +0200

    add main-coponenet to  admin  module

--- Frontend/Cantine/src/app/admin/admin.module.ts
+++ Frontend/Cantine/src/app/admin/admin.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainAdminComponent } from './main-admin/main-admin.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainAdminComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ Frontend/Cantine/src/app/admin/main-admin/main-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-admin',
+  templateUrl: './main-admin.component.html',
+  styles: [
+  ]
+})
+export class MainAdminComponent {
+
+}
