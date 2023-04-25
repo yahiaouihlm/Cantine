c92294a52a5bb8fd059bf2036dce5c29dc9ecdfb
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-25 23:34:43 +0200

    add No MealInMenuException

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/exceptions/NoMealInMenuException.java
@@ -0,0 +1,8 @@
+package fr.sqli.Cantine.service.admin.menus.exceptions;
+
+public class NoMealInMenuException extends Exception{
+
+    public NoMealInMenuException(String message) {
+        super(message);
+    }
+}
