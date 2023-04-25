e10209f0d5940efb9f045eb7a600748433c6f082
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-25 23:52:00 +0200

    check meals validity in menu

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/IMenuService.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/IMenuService.java
@@ -1,12 +1,16 @@
 package fr.sqli.Cantine.service.admin.menus;
 
+import fr.sqli.Cantine.dto.in.MenuDtoIn;
 import fr.sqli.Cantine.entity.MenuEntity;
+import fr.sqli.Cantine.service.admin.meals.exceptions.InvalidMealInformationException;
+import fr.sqli.Cantine.service.admin.meals.exceptions.MealNotFoundAdminException;
+import fr.sqli.Cantine.service.admin.menus.exceptions.InvalidMenuInformationException;
 
 public interface IMenuService {
 
 
 
-  public MenuEntity addMenu(MenuEntity menuEntity) ;
+  public MenuEntity addMenu(MenuDtoIn menuDtoIn) throws InvalidMenuInformationException, InvalidMealInformationException, MealNotFoundAdminException;
 
 
 
--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/MenuSerive.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/MenuSerive.java
@@ -1,15 +1,41 @@
 package fr.sqli.Cantine.service.admin.menus;
 
 
+import fr.sqli.Cantine.dto.in.MenuDtoIn;
+import fr.sqli.Cantine.entity.MealEntity;
 import fr.sqli.Cantine.entity.MenuEntity;
+import fr.sqli.Cantine.service.admin.meals.MealService;
+import fr.sqli.Cantine.service.admin.meals.exceptions.InvalidMealInformationException;
+import fr.sqli.Cantine.service.admin.meals.exceptions.MealNotFoundAdminException;
+import fr.sqli.Cantine.service.admin.menus.exceptions.InvalidMenuInformationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuSerive implements  IMenuService {
 
+    MealService mealService;
+    @Autowired
+    public  MenuSerive(MealService mealService) {
+        this.mealService = mealService;
+    }
 
     @Override
-    public MenuEntity addMenu(MenuEntity menuEntity) {
-        return null;
+    public MenuEntity addMenu(MenuDtoIn menuDtoIn) throws InvalidMenuInformationException, InvalidMealInformationException, MealNotFoundAdminException {
+          var menuEntity  =  menuDtoIn.toMenuEntity();
+
+          if  (menuDtoIn.getMealIDs()== null || menuDtoIn.getMealIDs().size() == 0 ||menuDtoIn.getMealIDs().isEmpty()) {
+              throw new InvalidMenuInformationException("The menu doesn't contain any meal");
+          }
+
+          for  (Integer mealID : menuDtoIn.getMealIDs()) {
+                    this.mealService.getMealByID(mealID);
+          }
+
+
+
+        return  null ;
     }
+
+
 }
