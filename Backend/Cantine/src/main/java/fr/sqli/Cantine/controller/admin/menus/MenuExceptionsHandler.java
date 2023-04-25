6531ae1450e3d756beffc3198d788278661b2e35
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-25 23:37:39 +0200

    add NoMealInMenuHandler Exception

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/menus/MenuExceptionsHandler.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/menus/MenuExceptionsHandler.java
@@ -2,6 +2,7 @@ package fr.sqli.Cantine.controller.admin.menus;
 
 import fr.sqli.Cantine.dto.out.ExceptionDtout;
 import fr.sqli.Cantine.service.admin.menus.exceptions.InvalidMenuInformationException;
+import fr.sqli.Cantine.service.admin.menus.exceptions.NoMealInMenuException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,6 +10,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class MenuExceptionsHandler {
+
+
+    /**
+     * Handle NoMealInMenuException exception when the menu doesn't contain any meal in it
+     * @param e NoMealInMenuException exception when the menu doesn't contain any meal in it
+     * @return ResponseEntity<ExceptionDtout> with the message of the exception
+     */
+
+    @ExceptionHandler(NoMealInMenuException.class)
+    public ResponseEntity<ExceptionDtout> handleNoMealInMenu(NoMealInMenuException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage()));
+    }
+
 
     /**
      *  Handle InvalidMenuInformationException exception when the menu information is invalid
@@ -20,4 +34,6 @@ public class MenuExceptionsHandler {
     public ResponseEntity<ExceptionDtout> handleExistingMenu(InvalidMenuInformationException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage()));
     }
+
+
 }
