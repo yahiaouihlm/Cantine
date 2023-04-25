66a17a3633c39230d12e21610ea1008684e8e53d
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-25 23:18:16 +0200

    make menu service

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/MenuSerive.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/menus/MenuSerive.java
@@ -1,2 +1,15 @@
-package fr.sqli.Cantine.service.admin.menus;public class MealService {
+package fr.sqli.Cantine.service.admin.menus;
+
+
+import fr.sqli.Cantine.entity.MenuEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuSerive implements  IMenuService {
+
+
+    @Override
+    public MenuEntity addMenu(MenuEntity menuEntity) {
+        return null;
+    }
 }
