26a9f77deeb5c159df821227187d4695d63f34c7
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-27 16:53:41 +0200

    add price and quantity  to   new Meal  Form

--- Frontend/Cantine/src/app/admin/admin.module.ts
+++ Frontend/Cantine/src/app/admin/admin.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainAdminComponent } from './main-admin/main-admin.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatIconModule} from "@angular/material/icon";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatListModule} from "@angular/material/list";
-import {MatBadgeModule} from "@angular/material/badge";
 import {RouterModule, Routes} from "@angular/router";
-import {CoreCantineModule} from "../core-cantine/core-cantine.module";
 import { HomeAdminComponent } from './home-admin/home-admin.component';
 
 import {SharedmoduleModule} from "../sharedmodule/sharedmodule.module";
