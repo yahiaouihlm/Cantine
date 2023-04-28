e77d4036f22796f8dd3726c4dd7346ab0435c22f
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-28 11:17:00 +0200

    add  addMenu unitTest with  Invalid Price

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/service/admin/menus/AddMenuTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/service/admin/menus/AddMenuTest.java
@@ -7,9 +7,9 @@ import fr.sqli.Cantine.entity.MenuEntity;
 import fr.sqli.Cantine.service.admin.meals.MealService;
 import fr.sqli.Cantine.service.admin.menus.exceptions.InvalidMenuInformationException;
 import fr.sqli.Cantine.service.images.IImageService;
-import fr.sqli.Cantine.service.images.ImageService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -20,7 +20,6 @@ import org.springframework.mock.env.MockEnvironment;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Collections;
@@ -60,6 +59,51 @@ public class AddMenuTest {
         this.menu.setMealIDs(Collections.singletonList(1));
 
     }
+
+    /*************************************** Price ******************************************/
+
+    @Test
+    void AddMenuWithTooHeightPriceTest(){
+        this.menu.setPrice(new BigDecimal(1001   ));
+        Assertions.assertThrows(InvalidMenuInformationException.class , () -> this.menuService.addMenu(this.menu));
+        Mockito.verify(iMenuDao, Mockito.times(0)).save(Mockito.any());
+
+    }
+    @Test
+    void AddMenuWithZeroPriceTest(){
+        this.menu.setPrice( new BigDecimal(0));
+        Assertions.assertThrows(InvalidMenuInformationException.class , () -> this.menuService.addMenu(this.menu));
+        Mockito.verify(iMenuDao, Mockito.times(0)).save(Mockito.any());
+
+    }
+    @Test
+    @Disabled
+    void AddMenuWithNegativePriceTest(){
+        this.menu.setPrice(new BigDecimal("-1"));
+        Assertions.assertThrows(InvalidMenuInformationException.class , () -> this.menuService.addMenu(this.menu));
+        Mockito.verify(iMenuDao, Mockito.times(0)).save(Mockito.any());
+
+    }
+
+    @Test
+    void AddMenuWithNullPriceTest () {
+        this.menu.setPrice(null);
+        Assertions.assertThrows(InvalidMenuInformationException.class , () -> this.menuService.addMenu(this.menu));
+        Mockito.verify(iMenuDao, Mockito.times(0)).save(Mockito.any());
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
   /*************************************** Description ******************************************/
   @Test
   void AddMenuWithTooShortDescriptionTest (){
