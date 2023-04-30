8f410a6d8a5f29d05e56edfdd536efce540f3a9b
yahiaouihlm <yahiaouihlm@gmail.com>
2023-04-30 20:11:29 +0200

    make interface method  to create meal and  menus

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/menus/IMenuTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/menus/IMenuTest.java
@@ -1,5 +1,11 @@
 package fr.sqli.Cantine.controller.admin.menus;
 
+import fr.sqli.Cantine.entity.ImageEntity;
+import fr.sqli.Cantine.entity.MealEntity;
+import fr.sqli.Cantine.entity.MenuEntity;
+
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
 
 public interface IMenuTest {
@@ -19,6 +25,7 @@ public interface IMenuTest {
             Map.entry("ShortLabelLength", "LABEL_IS_TOO_SHORT"),
             Map.entry("Description", "DESCRIPTION_IS_MANDATORY"),
             Map.entry("Price", "PRICE_IS_MANDATORY"),
+            Map.entry("Image", "IMAGE_IS_MANDATORY"),
             Map.entry("ShortDescriptionLength", "DESCRIPTION_IS_TOO_SHORT"),
             Map.entry("LongDescriptionLength", "DESCRIPTION_IS_TOO_LONG"),
             Map.entry("Status", "STATUS_IS_MANDATORY"),
@@ -27,8 +34,40 @@ public interface IMenuTest {
             Map.entry("HighQuantity", "QUANTITY_IS_TOO_HIGH"),
             Map.entry("NegativeQuantity", "QUANTITY MUST BE GREATER THAN 0"),
             Map.entry("HighPrice", "PRICE MUST BE LESS THAN 1000"),
-            Map.entry("NegativePrice", "PRICE MUST BE GREATER THAN 0")
+            Map.entry("NegativePrice", "PRICE MUST BE GREATER THAN 0"),
+            Map.entry("InvalidImageFormat", "INVALID IMAGE TYPE ONLY PNG , JPG , JPEG   ARE ACCEPTED")
 
 
     );
+
+    static  MealEntity createMeal  () {
+
+        ImageEntity imageEntity = new ImageEntity();
+        imageEntity.setImagename(IMAGE_MENU_FOR_TEST_NAME);
+
+        MealEntity mealEntity = createMealWith("MealTest" , new BigDecimal(10.0) , "MealTest  description" , 1 , 10 , imageEntity);
+
+        return  mealEntity ;
+    }
+
+     static  MealEntity createMealWith( String  label , BigDecimal price , String description , int status , int quantity , ImageEntity imageEntity){
+        MealEntity mealEntity = new MealEntity();
+        mealEntity.setLabel(label);
+        mealEntity.setPrice(price);
+        mealEntity.setDescription(description);
+        mealEntity.setStatus(status);
+        mealEntity.setQuantity(quantity);
+        mealEntity.setImage(imageEntity);
+        return  mealEntity ;
+    }
+    static MenuEntity createMenu (List <MealEntity> mealAssociated ) {
+        MenuEntity menuEntity = new MenuEntity();
+        menuEntity.setLabel("Tacos");
+        menuEntity.setPrice(new BigDecimal("3.87"));
+        menuEntity.setDescription("Menu  description  of Tacos menu");
+        menuEntity.setStatus(1);
+        menuEntity.setQuantity(10);
+        menuEntity.setMeals(mealAssociated);
+        return  menuEntity ;
+    }
 }
