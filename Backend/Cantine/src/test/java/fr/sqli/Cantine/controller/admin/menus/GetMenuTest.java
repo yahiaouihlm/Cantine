3f613ef41dd448520eee25bb5d9dd0c35de23cdb
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-01 23:25:19 +0200

    add GetMenu Container Tests

---
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/menus/GetMenuTest.java
@@ -0,0 +1,6 @@
+package fr.sqli.Cantine.controller.admin.menus;
+
+import fr.sqli.Cantine.controller.admin.AbstractContainerConfig;
+
+public class GetMenuTest  extends AbstractContainerConfig implements  IMenuTest {
+}
