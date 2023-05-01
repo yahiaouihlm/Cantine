9b4617da95082c688a1c76e299ac81a425c20646
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-01 14:11:24 +0200

    add RemoveMenu Container Tests

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/menus/RemoveMenuTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/admin/menus/RemoveMenuTest.java
@@ -1,2 +1,28 @@
-package fr.sqli.Cantine.controller.admin.menus;public class RemoveMenuTest {
+package fr.sqli.Cantine.controller.admin.menus;
+
+
+import fr.sqli.Cantine.controller.admin.AbstractContainerConfig;
+import fr.sqli.Cantine.dao.IMenuDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RemoveMenuTest extends AbstractContainerConfig implements IMenuTest {
+
+    @Autowired
+    private IMenuDao menuDao;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+   @Test
+  void removeMenuWithOutIdTest() {
+
+  }
+
 }
