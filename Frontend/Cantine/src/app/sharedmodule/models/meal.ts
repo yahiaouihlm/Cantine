5aa1afbc6a6b1b26a801a8900074032bc315cb40
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 13:22:09 +0200

    add Meal model

---
+++ Frontend/Cantine/src/app/sharedmodule/models/meal.ts
@@ -0,0 +1,15 @@
+export  class  Meal {
+    id! :  number;
+    label! :  string;
+    description! :  string;
+    category! :  string;
+    price! :  number;
+
+    quantity! :  number;
+
+    status! :  number;
+
+    image! :  string;
+
+
+}
