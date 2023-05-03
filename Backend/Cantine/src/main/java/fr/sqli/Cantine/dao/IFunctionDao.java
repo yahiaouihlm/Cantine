f00c3dbc978b16ef2e6e92ab5fec34885401772e
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 18:43:00 +0200

    add IFunctionDao

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IFunctionDao.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IFunctionDao.java
@@ -1,2 +1,10 @@
-package fr.sqli.Cantine.dao;public class IFunctionDao {
+package fr.sqli.Cantine.dao;
+
+
+import fr.sqli.Cantine.entity.FunctionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IFunctionDao extends JpaRepository<FunctionEntity, Integer> {
 }
