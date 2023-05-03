ba4e9e068a94d50efcf379aaecf2bde90fd69afc
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 23:39:46 +0200

    add InvalidEmailAdminException

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/adminDashboard/exceptions/InvalidEmailAdminException.java
@@ -0,0 +1,7 @@
+package fr.sqli.Cantine.service.admin.adminDashboard.exceptions;
+
+public class InvalidEmailAdminException extends Exception {
+    public InvalidEmailAdminException(String message) {
+        super(message);
+    }
+}
