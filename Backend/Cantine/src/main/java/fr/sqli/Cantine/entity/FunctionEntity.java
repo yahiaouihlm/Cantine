00891356e788e9c0255f56f3739157a3e5972232
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 18:32:54 +0200

    update Function  Entity

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/entity/FunctionEntity.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/entity/FunctionEntity.java
@@ -1,2 +1,33 @@
-package fr.sqli.Cantine.entity;public class FunctionEntity {
+package fr.sqli.Cantine.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="function")
+public class FunctionEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(unique=true, nullable=false)
+    private Integer id;
+
+    @Column(name = "name",  nullable=false, length=99)
+    private  String name  ;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
