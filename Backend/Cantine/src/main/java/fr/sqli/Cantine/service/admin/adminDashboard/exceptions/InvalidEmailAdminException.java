dbf0da8eadb93d9dfe30be156054d56316e3d6f9
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 23:53:09 +0200

    update

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/adminDashboard/exceptions/InvalidEmailAdminException.java
+++
@@ -1,7 +0,0 @@
-package fr.sqli.Cantine.service.admin.adminDashboard.exceptions;
-
-public class InvalidEmailAdminException extends Exception {
-    public InvalidEmailAdminException(String message) {
-        super(message);
-    }
-}
