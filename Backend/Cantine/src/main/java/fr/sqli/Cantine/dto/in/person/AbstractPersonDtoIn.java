858eee33deab9cb2d5baee85152bb63f15dd286e
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 22:30:29 +0200

    make user/admin inforation validator

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
@@ -1,18 +1,73 @@
 package fr.sqli.Cantine.dto.in.person;
 
+import fr.sqli.Cantine.service.admin.registration.exceptions.InvalidPersonInformationException;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
 
 public  abstract  class AbstractPersonDtoIn {
     private  String firstname;
     private  String lastname;
     private  String email;
     private  String password;
-    private  String birthdate;
+    private  String birthdateAsString;
+    private LocalDate birthdate;
     private  String towen;
     private  String adresse;
     private  String phone;
     private MultipartFile image;
 
+    public  void ValidatePersonInformationWithOutImageAndPhone() throws InvalidPersonInformationException {
+        if  (this.firstname == null || this.firstname.isEmpty() || this.firstname.isBlank())
+            throw new InvalidPersonInformationException("FIRSTNAME IS  REQUIRED");
+
+        if  (this.lastname == null || this.lastname.isEmpty() || this.lastname.isBlank())
+            throw new InvalidPersonInformationException("LASTNAME IS  REQUIRED");
+
+        if (this.email == null || this.email.isEmpty() || this.email.isBlank())
+            throw new InvalidPersonInformationException("EMAIL IS  REQUIRED");
+
+        if (this.password == null || this.password.isEmpty() || this.password.isBlank())
+            throw new InvalidPersonInformationException("PASSWORD IS  REQUIRED");
+
+
+            birthdateValidator ();
+
+        if (this.towen == null || this.towen.isEmpty() || this.towen.isBlank())
+            throw new InvalidPersonInformationException("TOWEN IS  REQUIRED");
+
+        if (this.adresse == null || this.adresse.isEmpty() || this.adresse.isBlank())
+            throw new InvalidPersonInformationException("ADRESSE IS  REQUIRED");
+
+    }
+
+
+    private   void birthdateValidator () throws InvalidPersonInformationException {
+        if (this.birthdateAsString != null && !this.birthdateAsString.isEmpty() && !this.birthdateAsString.isBlank())
+              throw  new InvalidPersonInformationException("BIRTHDATE IS  REQUIRED");
+        try {
+            this.birthdate = LocalDate.parse(this.birthdateAsString);
+        }catch (Exception e){
+            throw  new InvalidPersonInformationException(" INVALID BIRTHDATE FORMAT");
+        }
+    }
+
+    protected void phoneValidator () throws InvalidPersonInformationException {
+
+        if (this.phone == null || this.phone.isEmpty() || this.phone.isBlank())
+            throw new InvalidPersonInformationException("PHONE IS  REQUIRED");
+
+        String regex = "^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$";
+
+        if (!this.phone.matches(regex))
+            throw new InvalidPersonInformationException("INVALID PHONE FORMAT");
+
+    }
+
+    protected void imageValidator () throws InvalidPersonInformationException {
+        if (this.image == null || this.image.isEmpty() || this.image.isEmpty())
+            throw new InvalidPersonInformationException("IMAGE IS  REQUIRED");
+    }
 
 
 
@@ -49,11 +104,19 @@ public  abstract  class AbstractPersonDtoIn {
         this.password = password;
     }
 
-    public String getBirthdate() {
+    public String getBirthdateAsString() {
+        return birthdateAsString;
+    }
+
+    public void setBirthdateAsString(String birthdateAsString) {
+        this.birthdateAsString = birthdateAsString;
+    }
+
+    public LocalDate getBirthdate() {
         return birthdate;
     }
 
-    public void setBirthdate(String birthdate) {
+    public void setBirthdate(LocalDate birthdate) {
         this.birthdate = birthdate;
     }
 
