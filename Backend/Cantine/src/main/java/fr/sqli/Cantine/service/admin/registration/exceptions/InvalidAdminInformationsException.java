d321bb59139f06bc9a551fb395142968cfdd62ed
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 19:49:44 +0200

    refactor AdminInformation Exception

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/registration/exceptions/InvalidAdminInformationsException.java
+++
@@ -1,7 +0,0 @@
-package fr.sqli.Cantine.service.admin.registration.exceptions;
-
-public class InvalidAdminInformationsException extends  Exception {
-    public InvalidAdminInformationsException(String message) {
-        super(message);
-    }
-}
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/admin/registration/exceptions/InvalidPersonInformationException.java
@@ -0,0 +1,7 @@
+package fr.sqli.Cantine.service.admin.registration.exceptions;
+
+public class InvalidPersonInformationException extends  Exception {
+    public InvalidPersonInformationException(String message) {
+        super(message);
+    }
+}
