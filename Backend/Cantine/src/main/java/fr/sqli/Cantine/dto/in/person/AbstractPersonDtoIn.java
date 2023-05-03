1be2962e2aa0307ee505ab7accb9e9c2ade4be46
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 19:26:37 +0200

    add  the shared feild to  AbsractPersonDtoIn

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
@@ -1,4 +1,87 @@
 package fr.sqli.Cantine.dto.in.person;
 
-public class AbstractPersonDtoIn {
+import org.springframework.web.multipart.MultipartFile;
+
+public  abstract  class AbstractPersonDtoIn {
+    private  String firstname;
+    private  String lastname;
+    private  String email;
+    private  String password;
+    private  String birthdate;
+    private  String towen;
+    private  String adresse;
+    private  String phone;
+    private MultipartFile image;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getTowen() {
+        return towen;
+    }
+
+    public void setTowen(String towen) {
+        this.towen = towen;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
 }
