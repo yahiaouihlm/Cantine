7247a817596185d7859a8183280ba455e59d84a0
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-03 23:05:24 +0200

    add  Bcrypt Bean  To crypte password

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/CantineApplication.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/CantineApplication.java
@@ -3,6 +3,7 @@ package fr.sqli.Cantine;
 
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -59,7 +60,13 @@ public class CantineApplication {
 		};
 	}
 
+
 	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder (){
+		return  new BCryptPasswordEncoder();
+	}
+
+/*	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
@@ -68,6 +75,6 @@ public class CantineApplication {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
-	}
+	}*/
 
 }
