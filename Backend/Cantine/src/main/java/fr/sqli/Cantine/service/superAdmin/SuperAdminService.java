0a1e1f216c4028872173da274a294f0050f4017d
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-04 18:06:13 +0200

    add  add Function  to  service

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/SuperAdminService.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/SuperAdminService.java
@@ -2,7 +2,9 @@ package fr.sqli.Cantine.service.superAdmin;
 
 
 import fr.sqli.Cantine.dao.IFunctionDao;
+import fr.sqli.Cantine.dto.in.superAdmin.FunctionDtoIn;
 import fr.sqli.Cantine.entity.FunctionEntity;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +19,10 @@ public class SuperAdminService {
     }
 
 
-    public FunctionEntity  addFunction (  Fun){
-        return  null;
+    public FunctionEntity  addFunction (FunctionDtoIn functionDtoIn) throws InvalidPersonInformationException {
+        FunctionEntity functionEntity = new FunctionEntity();
+        functionEntity.setName(functionDtoIn.getName());
+        return  this.iFunctionDao.save(functionEntity);
     }
 
 
