ca8e58d2c582c0ec200e40b11ca00309ecd6df6e
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-04 18:44:05 +0200

    add admin controller  for  signUp

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/AdminController.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/AdminController.java
@@ -1,2 +1,28 @@
-package fr.sqli.Cantine.controller.admin.adminDashboard;public class AdminController {
+package fr.sqli.Cantine.controller.admin.adminDashboard;
+
+import fr.sqli.Cantine.dto.in.person.AdminDtoIn;
+import fr.sqli.Cantine.service.admin.adminDashboard.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static fr.sqli.Cantine.controller.admin.adminDashboard.IAdminDashboardController.ADMIN_DASH_BOARD_SIGN_UP_ENDPOINT;
+
+@RestController
+@RequestMapping(ADMIN_DASH_BOARD_SIGN_UP_ENDPOINT)
+public class AdminController  implements IAdminDashboardController {
+
+    private AdminService adminService;
+
+    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+
+    @Override
+    public ResponseEntity<String> signUp(AdminDtoIn adminDtoIn) {
+        this.adminService.signUp(adminDtoIn);
+    }
 }
--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/IAdminDashboardController.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/IAdminDashboardController.java
@@ -1,6 +1,15 @@
 package fr.sqli.Cantine.controller.admin.adminDashboard;
 
-public interface IAdminDashboard {
+import fr.sqli.Cantine.dto.in.person.AdminDtoIn;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public interface IAdminDashboardController {
 
     String ADMIN_DASH_BOARD_BASIC_URL = "/cantine/admin/adminDashboard";
+
+    String ADMIN_DASH_BOARD_SIGN_UP_ENDPOINT =  "/signUp";
+
+
+    public ResponseEntity<String> signUp(@ModelAttribute AdminDtoIn adminDtoIn);
 }
