6681efde1d262a1bc512084cdb7f32a841bfb715
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-04 19:01:21 +0200

    add InvalidPersonException Handler for  Admin  Registration

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/AdminExceptionHandler.java
@@ -0,0 +1,21 @@
+package fr.sqli.Cantine.controller.admin.adminDashboard;
+
+
+import fr.sqli.Cantine.dto.out.ExceptionDtout;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
+import fr.sqli.Cantine.service.admin.meals.exceptions.ExistingMealException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class AdminExceptionHandler {
+
+    @ExceptionHandler(InvalidPersonInformationException.class)
+    public ResponseEntity<ExceptionDtout> handleExistingMeal(InvalidPersonInformationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage()));
+    }
+
+
+}
