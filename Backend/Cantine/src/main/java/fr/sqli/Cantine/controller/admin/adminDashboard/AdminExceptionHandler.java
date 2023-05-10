ae08f283ff7eb65d1748f791933986b93ec1a0e2
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-10 13:23:03 +0200

    add Exisdting Admin  Exception  Handler

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/AdminExceptionHandler.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/AdminExceptionHandler.java
@@ -2,6 +2,7 @@ package fr.sqli.Cantine.controller.admin.adminDashboard;
 
 
 import fr.sqli.Cantine.dto.out.ExceptionDtout;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.ExistingAdminException;
 import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
 import fr.sqli.Cantine.service.admin.meals.exceptions.ExistingMealException;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class AdminExceptionHandler {
 
+
+
+    @ExceptionHandler(ExistingAdminException.class)
+    public ResponseEntity<ExceptionDtout> handleExistingAdmin(ExistingAdminException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ExceptionDtout(e.getMessage()));
+    }
     @ExceptionHandler(InvalidPersonInformationException.class)
     public ResponseEntity<ExceptionDtout> handleExistingMeal(InvalidPersonInformationException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage()));
