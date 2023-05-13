cba57c7e23347af88bad998734e66a3441ae5d92
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-13 16:40:33 +0200

    update regex phone validator

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/person/AbstractPersonDtoIn.java
@@ -106,7 +106,7 @@ public  abstract  class AbstractPersonDtoIn {
             throw new InvalidPersonInformationException("PHONE IS  REQUIRED");
 
 
-        String regex = "^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s.-]*\\d{2}){4}$";
+        String regex = "^(?:(?:\\+|00)33[\\s.-]{0,3}(?:\\(0\\)[\\s.-]{0,3})?|0)[1-9](?:(?:[\\s.-]?\\d{2}){4}|\\d{2}(?:[\\s.-]?\\d{3}){2})$";
 
         if (!this.phone.matches(regex))
             throw new InvalidPersonInformationException("INVALID PHONE FORMAT");
