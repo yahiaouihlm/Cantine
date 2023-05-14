46af27861e6ffeaf401bf2dbc9a6eb89ee52320d
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-14 18:23:04 +0200

    add AdminDtout

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/out/person/AdminDtout.java
@@ -0,0 +1,33 @@
+package fr.sqli.Cantine.dto.out.person;
+
+import fr.sqli.Cantine.entity.AdminEntity;
+
+public class AdminDtout extends AbstractPersonDtout{
+
+   private  String function;
+
+    public AdminDtout(AdminEntity adminEntity){
+            super.setId(adminEntity.getId());
+            super.setFirstname(adminEntity.getFirstname());
+            super.setLastname(adminEntity.getLastname());
+            super.setEmail(adminEntity.getEmail());
+            super.setBirthdate(adminEntity.getBirthdate());
+            super.setAddress(adminEntity.getAddress());
+            super.setTown(adminEntity.getTown());
+            super.setPhone(adminEntity.getPhone());
+            super.setImage(adminEntity.getImage().getImagename());
+            this.setFunction(adminEntity.getFunction().getName());
+    }
+
+
+
+
+    public String getFunction() {
+        return Function;
+    }
+
+    public void setFunction(String function) {
+        Function = function;
+    }
+
+}
