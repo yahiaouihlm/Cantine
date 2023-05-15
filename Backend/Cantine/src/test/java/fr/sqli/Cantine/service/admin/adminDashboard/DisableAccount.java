cd65a19a9f337166d1d8aa1e08a4f011f244e219
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-15 12:38:35 +0200

    add DisabledAdminAcut  With  Invalid  Id unit  Tests

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/service/admin/adminDashboard/DisableAccount.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/service/admin/adminDashboard/DisableAccount.java
@@ -3,15 +3,27 @@ package fr.sqli.Cantine.service.admin.adminDashboard;
 import fr.sqli.Cantine.dao.IAdminDao;
 import fr.sqli.Cantine.dao.IFunctionDao;
 import fr.sqli.Cantine.dto.in.person.AdminDtoIn;
+import fr.sqli.Cantine.entity.AdminEntity;
 import fr.sqli.Cantine.entity.FunctionEntity;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.AdminNotFound;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
 import fr.sqli.Cantine.service.images.ImageService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.env.MockEnvironment;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class DisableAccount {
@@ -19,17 +31,68 @@ public class DisableAccount {
     final   String  IMAGE_TESTS_PATH = "imagesTests/ImageForTest.jpg";
     @Mock
     private IAdminDao adminDao;
-    @Mock
-    private ImageService imageService;
 
     @Mock
     private IFunctionDao functionDao;
-    @Mock
-    private MockEnvironment environment;
+
     @InjectMocks
     private AdminService adminService;
+
+    @Mock
+     private  MockEnvironment env;
     private FunctionEntity functionEntity;
-    private AdminDtoIn adminDtoIn;
+    private  AdminEntity adminEntity;
+
+
+    @BeforeEach
+    void  init  () {
+         this.functionEntity = new FunctionEntity();
+        functionEntity.setId(1);
+        functionEntity.setName("name");
+
+         this.adminEntity = new AdminEntity();
+        adminEntity.setId(1);
+
+        adminEntity.setFirstname("firstname");
+        adminEntity.setLastname("lastname");
+        adminEntity.setEmail("email");
+        adminEntity.setPassword("password");
+        adminEntity.setBirthdate(LocalDate.now());
+        adminEntity.setTown("town");
+        adminEntity.setAddress("address");
+        adminEntity.setFunction(this.functionEntity);
+    }
+
+
+    /******************************** TESTS  ADMIN ID  ********************************/
+
+    @Test
+    void  disabledAccountWithNotFoundAdmin () throws InvalidPersonInformationException {
+        Integer idMenu = 1 ;
+
+
+        Mockito.when(this.adminDao.findById(idMenu)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(AdminNotFound.class, () -> {
+            this.adminService.disableAdminAccount(idMenu);
+        });
+
+        Mockito.verify(this.adminDao, Mockito.times(1)).findById(idMenu);
+
+    }
+    @Test
+    void disabledAccountWithNegativeID (){
+        assertThrows(InvalidPersonInformationException.class, () -> {
+            this.adminService.disableAdminAccount( -1);
+        });
+    }
+    @Test
+    void disabledAccountWithNullID(){
+        assertThrows(InvalidPersonInformationException.class, () -> {
+            this.adminService.disableAdminAccount( null);
+        });
+    }
+
 
 
 
