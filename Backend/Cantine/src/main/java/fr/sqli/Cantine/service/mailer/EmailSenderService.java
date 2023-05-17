d3e01248a948c32bf310bede2c5fa4801e7b28d5
yahiaouihlm <yahiaouihlm@gmail.com>
2023-05-17 19:42:33 +0200

    add  mailer sender  dependency  to  Pom.xml

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/mailer/EmailSenderService.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/mailer/EmailSenderService.java
@@ -1,4 +1,25 @@
 package fr.sqli.Cantine.service.mailer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailSenderService {
+
+
+    private JavaMailSender javaMailSender ;
+
+    @Autowired
+    public  EmailSenderService ( JavaMailSender javaMailSender){
+        this.javaMailSender =  javaMailSender;
+    }
+
+
+    @Async
+    public void sendEmail (SimpleMailMessage message){
+        javaMailSender.send(message);
+    }
+
+
 }
