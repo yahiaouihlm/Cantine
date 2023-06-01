6b8dfb09f3282768f93308d9a7a0906b7eeec518
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-01 19:43:22 +0200

    add addStudnt  class  controller

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/works/AdminWorksController.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/works/AdminWorksController.java
@@ -1,4 +1,28 @@
 package fr.sqli.Cantine.controller.admin.adminDashboard.works;
 
-public class AdminWorksController {
+
+import fr.sqli.Cantine.dto.in.person.StudentClassDtoIn;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidStudentClassException;
+import fr.sqli.Cantine.service.admin.adminDashboard.work.AdminWorksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(IAdminWorksController.ADMIN_DASH_BOARD_BASIC_WORK_URL)
+public class AdminWorksController  implements  IAdminWorksController {
+
+    private AdminWorksService adminWorksService;
+
+    @Autowired
+    public  AdminWorksController  (AdminWorksService adminWorksService){
+        this.adminWorksService = adminWorksService;
+    }
+
+    @Override
+    @PostMapping(ADD_STUDENT_CLASS_ENDPOINT)
+    public ResponseEntity<String> addStudentClass(@RequestBody StudentClassDtoIn studentClassDtoIn) throws InvalidStudentClassException {
+        this.adminWorksService.addStudentClass(studentClassDtoIn);
+        return ResponseEntity.ok(STUDENT_CLASS_ADDED_SUCCESSFULLY);
+    }
 }
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/admin/adminDashboard/works/IAdminWorksController.java
@@ -0,0 +1,17 @@
+package fr.sqli.Cantine.controller.admin.adminDashboard.works;
+
+
+import fr.sqli.Cantine.dto.in.person.StudentClassDtoIn;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidStudentClassException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface IAdminWorksController {
+
+    String ADMIN_DASH_BOARD_BASIC_WORK_URL = "/cantine/admin/adminDashboard/works";
+
+    String  ADD_STUDENT_CLASS_ENDPOINT = "/addStudentClass";
+
+    String STUDENT_CLASS_ADDED_SUCCESSFULLY = "STUDENT CLASS ADDED SUCCESSFULLY";
+    public ResponseEntity<String> addStudentClass(@RequestBody StudentClassDtoIn studentClassDtoIn) throws InvalidStudentClassException;
+}
