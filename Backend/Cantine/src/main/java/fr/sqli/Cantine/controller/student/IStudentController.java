ef2a9e1e73c1ac119f537a28cbf8a8fd884b2b91
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-08 10:59:13 +0200

    add   student Sign Up  Controller

---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/student/IStudentController.java
@@ -0,0 +1,24 @@
+package fr.sqli.Cantine.controller.student;
+
+import fr.sqli.Cantine.dto.in.person.StudentDtoIn;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidStudentClassException;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.StudentClassNotFoundException;
+import fr.sqli.Cantine.service.images.exception.ImagePathException;
+import fr.sqli.Cantine.service.images.exception.InvalidFormatImageException;
+import fr.sqli.Cantine.service.images.exception.InvalidImageException;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+
+public interface IStudentController {
+    String ADMIN_DASH_BOARD_BASIC_URL = "/cantine/student/";
+
+
+
+    String  STUDENT_SIGN_UP_ENDPOINT =  ADMIN_DASH_BOARD_BASIC_URL +  "/signUp";
+
+
+    String  STUDENT_SIGNED_UP_SUCCESSFULLY = "STUDENT SAVED SUCCESSFULLY";
+    ResponseEntity<String> signUpStudent(StudentDtoIn studentDtoIn) throws InvalidPersonInformationException, InvalidStudentClassException, InvalidFormatImageException, InvalidImageException, StudentClassNotFoundException, ImagePathException, IOException;
+}
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/student/StudentController.java
@@ -0,0 +1,37 @@
+package fr.sqli.Cantine.controller.student;
+
+import fr.sqli.Cantine.dto.in.person.StudentDtoIn;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidStudentClassException;
+import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.StudentClassNotFoundException;
+import fr.sqli.Cantine.service.images.exception.ImagePathException;
+import fr.sqli.Cantine.service.images.exception.InvalidFormatImageException;
+import fr.sqli.Cantine.service.images.exception.InvalidImageException;
+import fr.sqli.Cantine.service.student.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping(StudentController.ADMIN_DASH_BOARD_BASIC_URL)
+public class StudentController implements IStudentController {
+
+   private StudentService studentService;
+   @Autowired
+   public  StudentController( StudentService studentService) {
+        this.studentService = studentService;
+   }
+
+
+ @Override
+ @PostMapping(STUDENT_SIGN_UP_ENDPOINT)
+ public ResponseEntity<String> signUpStudent(@ModelAttribute StudentDtoIn studentDtoIn) throws InvalidPersonInformationException, InvalidStudentClassException, InvalidFormatImageException, InvalidImageException, StudentClassNotFoundException, ImagePathException, IOException {
+        this.studentService.signUpStudent(studentDtoIn);
+        return ResponseEntity.ok(STUDENT_SIGNED_UP_SUCCESSFULLY);
+ }
+}
