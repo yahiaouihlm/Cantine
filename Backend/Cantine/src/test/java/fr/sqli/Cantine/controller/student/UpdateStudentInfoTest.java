a9d30b90b87e135178d686924b3a7dd2fe07fdc1
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-10 20:46:10 +0200

    add Update  Student Info  Unit  Test

---
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/student/UpdateStudentInfoTest.java
@@ -0,0 +1,8 @@
+package fr.sqli.Cantine.controller.student;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+public class UpdateStudentInfoTest {
+
+}
