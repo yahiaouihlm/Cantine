da05c4216a35410d71ba4309355ce198fedc5df4
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 16:31:15 +0200

    add  tax   Service /  Controller

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminController.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminController.java
@@ -4,12 +4,13 @@ package fr.sqli.Cantine.controller.superAdmin;
 import fr.sqli.Cantine.dto.in.superAdmin.FunctionDtoIn;
 import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
 import fr.sqli.Cantine.service.superAdmin.SuperAdminService;
+import fr.sqli.Cantine.service.superAdmin.exception.ExistingTax;
+import fr.sqli.Cantine.service.superAdmin.exception.InvalidTaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/cantine/superAdmin")
@@ -22,11 +23,19 @@ public class SuperAdminController {
     }
 
 
-    @RequestMapping("/addFunction")
+    @PostMapping("/addFunction")
     public ResponseEntity<String> addFunction (@RequestBody FunctionDtoIn functionDtoIn) throws InvalidPersonInformationException {
         this.superAdminService.addFunction(functionDtoIn);
         return ResponseEntity.ok("function added successfully");
     }
+
+
+    @PostMapping("/addTax")
+    public ResponseEntity<String> addTax (@RequestParam("taxValue") BigDecimal taxValue) throws InvalidTaxException, ExistingTax {
+        this.superAdminService.addTax(taxValue);
+        return ResponseEntity.ok("tax added successfully");
+    }
+
 
 
 }
--- Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/SuperAdminService.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/SuperAdminService.java
@@ -7,6 +7,8 @@ import fr.sqli.Cantine.dto.in.superAdmin.FunctionDtoIn;
 import fr.sqli.Cantine.entity.FunctionEntity;
 import fr.sqli.Cantine.entity.TaxEntity;
 import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInformationException;
+import fr.sqli.Cantine.service.superAdmin.exception.ExistingTax;
+import fr.sqli.Cantine.service.superAdmin.exception.InvalidTaxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,7 +37,13 @@ public class SuperAdminService {
     }
 
 
-    public TaxEntity addTax(BigDecimal taxValue) {
+    public TaxEntity addTax(BigDecimal taxValue) throws InvalidTaxException, ExistingTax {
+        if (taxValue == null || taxValue.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new InvalidTaxException("TAX VALUE IS REQUIRED");
+        }
+        if  ((long) this.iTaxDao.findAll().size() > 0) {
+            throw  new ExistingTax("TAX VALUE ALREADY EXISTS");
+        }
         TaxEntity taxEntity = new TaxEntity();
         taxEntity.setTax(taxValue);
 
