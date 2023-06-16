7289832ec073228b64ecf3b8b687bf560088ab35
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 15:58:32 +0200

    update  DAO

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IOrderDao.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/IOrderDao.java
@@ -2,7 +2,9 @@ package fr.sqli.Cantine.dao;
 
 import fr.sqli.Cantine.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IOrderDao  extends JpaRepository<OrderEntity , Integer> {
 
 }
--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/ITaxDao.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dao/ITaxDao.java
@@ -2,7 +2,9 @@ package fr.sqli.Cantine.dao;
 
 import fr.sqli.Cantine.entity.TaxEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ITaxDao extends JpaRepository<TaxEntity, Integer> {
 
 }
