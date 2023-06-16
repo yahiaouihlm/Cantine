cbf7a1b9842bad8c3e3674fcfcdae189bc4a9282
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 16:27:29 +0200

    add  Invalid  Tax Exception

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminExceptionHandler.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminExceptionHandler.java
@@ -2,6 +2,7 @@ package fr.sqli.Cantine.controller.superAdmin;
 
 import fr.sqli.Cantine.dto.out.ExceptionDtout;
 import fr.sqli.Cantine.service.superAdmin.exception.ExistingTax;
+import fr.sqli.Cantine.service.superAdmin.exception.InvalidTaxException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class SuperAdminExceptionHandler {
 
 
-
+    @ExceptionHandler(InvalidTaxException.class)
+    public ResponseEntity<ExceptionDtout> handleHttpMessageNotReadableException(InvalidTaxException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage().toUpperCase()));
+    }
 
     @ExceptionHandler(ExistingTax.class)
     public ResponseEntity<ExceptionDtout> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/exception/InvalidTaxException.java
@@ -0,0 +1,8 @@
+package fr.sqli.Cantine.service.superAdmin.exception;
+
+public class InvalidTaxException extends  Exception{
+    public InvalidTaxException(String message) {
+        super(message);
+    }
+
+}
