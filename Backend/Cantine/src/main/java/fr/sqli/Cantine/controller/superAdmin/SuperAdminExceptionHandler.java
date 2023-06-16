e54feb8e15f7357b573daf405b7cf5dd67004bec
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 17:05:34 +0200

    add Tax Not  Found Exception

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminExceptionHandler.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/superAdmin/SuperAdminExceptionHandler.java
@@ -3,6 +3,7 @@ package fr.sqli.Cantine.controller.superAdmin;
 import fr.sqli.Cantine.dto.out.ExceptionDtout;
 import fr.sqli.Cantine.service.superAdmin.exception.ExistingTax;
 import fr.sqli.Cantine.service.superAdmin.exception.InvalidTaxException;
+import fr.sqli.Cantine.service.superAdmin.exception.TaxNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class SuperAdminExceptionHandler {
 
+    @ExceptionHandler(TaxNotFoundException.class)
+    public ResponseEntity<ExceptionDtout> handleHttpMessageNotReadableException(TaxNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ExceptionDtout(e.getMessage().toUpperCase()));
+    }
 
     @ExceptionHandler(InvalidTaxException.class)
     public ResponseEntity<ExceptionDtout> handleHttpMessageNotReadableException(InvalidTaxException e) {
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/superAdmin/exception/TaxNotFoundException.java
@@ -0,0 +1,8 @@
+package fr.sqli.Cantine.service.superAdmin.exception;
+
+public class TaxNotFoundException  extends  Exception{
+    public TaxNotFoundException(String message) {
+        super(message);
+    }
+}
+
