cde9e2ba9b90b0b463a89ed8e2e92fa7e19f3613
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 14:14:35 +0200

    add  getAdminByID container  Tests

--- Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/student/GetStudentTest.java
+++ Backend/Cantine/src/test/java/fr/sqli/Cantine/controller/student/GetStudentTest.java
@@ -1,2 +1,15 @@
-package fr.sqli.Cantine.controller.student;public class GetStudentTest {
+package fr.sqli.Cantine.controller.student;
+
+import fr.sqli.Cantine.controller.AbstractContainerConfig;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GetStudentTest  extends AbstractContainerConfig implements IStudentTest {
+    final  String paramReq = "?" + "idStudent" + "=";
+
+
+
 }
