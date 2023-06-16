803d9a41227006b19ed8b35c40c6d8ccb4c17884
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-16 18:36:02 +0200

    add  getter  and  setter  to  Order

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/entity/OrderEntity.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/entity/OrderEntity.java
@@ -27,6 +27,9 @@ public class OrderEntity implements Serializable {
     @Column(name = "creation_time", nullable=false)
     private Time creationTime;
 
+    @Column(name = "uuid", nullable=false)
+    private String uuid;
+
     @Column(name = "qr_code", nullable=false, length = 1000)
     private  String QRCode;
     @Column(nullable = false )
@@ -58,5 +61,83 @@ public class OrderEntity implements Serializable {
     private List<MealEntity> meals;
 
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Time getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Time creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getQRCode() {
+        return QRCode;
+    }
+
+    public void setQRCode(String QRCode) {
+        this.QRCode = QRCode;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public StudentEntity getStudent() {
+        return student;
+    }
+
+    public void setStudent(StudentEntity student) {
+        this.student = student;
+    }
+
+    public List<MenuEntity> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<MenuEntity> menus) {
+        this.menus = menus;
+    }
+
+    public List<MealEntity> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<MealEntity> meals) {
+        this.meals = meals;
+    }
 }
