290bb77c5a2b241c27272f66d214969fe0dbf1df
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-18 00:33:00 +0200

    add getAllMeals Service

--- Frontend/Cantine/src/app/core-cantine/core-cantine.service.ts
+++ Frontend/Cantine/src/app/core-cantine/core-cantine.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Meal} from "../sharedmodule/models/meal";
 
 @Injectable()
 export class CoreCantineService {
+  private  BASIC_MEAL_URL = "http://localhost:8080/cantine/api" ;
+  private  GET_ALL_MEALS_ENDPOINT = this.BASIC_MEAL_URL +  'meals/getAll'
 
-  constructor() { }
+  private GET_ALL_MENUS_ENDPOINT =  this.BASIC_MEAL_URL + 'menus/getAll';
+  constructor(private httpClient: HttpClient) { }
+
+   getAllMeals () {
+    return this.httpClient.get<Meal[]>(this.GET_ALL_MEALS_ENDPOINT)
+  }
+
 }
