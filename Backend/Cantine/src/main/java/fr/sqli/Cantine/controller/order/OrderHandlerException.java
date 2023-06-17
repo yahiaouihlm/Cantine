f6b1c617c42fb98d580c3e702483e4459014ca84
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-17 18:16:46 +0200

    add  Invalid  Order  Exception

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/order/OrderHandlerException.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/controller/order/OrderHandlerException.java
@@ -3,6 +3,7 @@ package fr.sqli.Cantine.controller.order;
 
 import fr.sqli.Cantine.dto.out.ExceptionDtout;
 import fr.sqli.Cantine.service.order.exception.InsufficientBalanceException;
+import fr.sqli.Cantine.service.order.exception.InvalidOrderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -13,8 +14,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class OrderHandlerException {
 
 
+
+    @ExceptionHandler(InvalidOrderException.class)
+    public ResponseEntity<ExceptionDtout> handleInvalidOrder(InvalidOrderException  e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionDtout(e.getMessage()));
+    }
+
+
     @ExceptionHandler(InsufficientBalanceException.class)
-    public ResponseEntity<ExceptionDtout> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    public ResponseEntity<ExceptionDtout> handleInsufficientBalance(HttpMessageNotReadableException e) {
         return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED).body(new ExceptionDtout(e.getMessage()));
     }
+
+
+
+
 }
---
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/service/order/exception/InvalidOrderException.java
@@ -0,0 +1,9 @@
+package fr.sqli.Cantine.service.order.exception;
+
+public class InvalidOrderException  extends   Exception{
+
+       public  InvalidOrderException( String  message ){
+           super(message);
+       }
+
+}
