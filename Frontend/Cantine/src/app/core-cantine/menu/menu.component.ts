c9607238268e020512846bb74f57072a96169f94
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-18 13:09:27 +0200

    add  Menu  Componenets

--- Frontend/Cantine/src/app/core-cantine/menu/menu.component.ts
+++ Frontend/Cantine/src/app/core-cantine/menu/menu.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CoreCantineService} from "../core-cantine.service";
+import {Observable, of} from "rxjs";
+import {Menu} from "../../sharedmodule/models/menu";
 
 @Component({
   selector: 'app-menu',
   templateUrl: "menu.component.html",
-  styles: [
-  ]
+  styles: [],
+  providers: [CoreCantineService]
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
 
+  menus$ : Observable<Menu[]> =  of([]);
+   constructor(private   coreCantineService  : CoreCantineService) { }
+
+    ngOnInit(): void {
+      this.menus$ = this.coreCantineService.getAllMenus();
+    }
 }
