a26265f8d673546c6ac9535bcb0f20cb92e22e80
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-18 23:55:02 +0200

    update check  Order Information Validity

--- Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/food/OrderDtoIn.java
+++ Backend/Cantine/src/main/java/fr/sqli/Cantine/dto/in/food/OrderDtoIn.java
@@ -5,6 +5,7 @@ import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidPersonInfo
 import fr.sqli.Cantine.service.admin.adminDashboard.exceptions.InvalidStudentClassException;
 import fr.sqli.Cantine.service.admin.meals.exceptions.InvalidMealInformationException;
 import fr.sqli.Cantine.service.admin.menus.exceptions.InvalidMenuInformationException;
+import fr.sqli.Cantine.service.order.exception.InvalidOrderException;
 
 import java.util.List;
 
@@ -17,25 +18,31 @@ public class OrderDtoIn {
     private List<Integer> menusId;
 
     @JsonIgnore
-    public void checkOrderIDsValidity() throws InvalidPersonInformationException, InvalidMealInformationException, InvalidMenuInformationException {
+    public void checkOrderIDsValidity() throws InvalidPersonInformationException, InvalidMealInformationException, InvalidMenuInformationException, InvalidOrderException {
         if (this.studentId == null || this.studentId < 0) {
             throw new InvalidPersonInformationException("STUDENT ID IS  REQUIRED");
         }
 
-        for (Integer mealId : mealsId) {
-            if (mealId == null || mealId < 0) {
-                throw new InvalidMealInformationException("INVALID MEAL ID");
-            }
+        if (this.mealsId == null && this.menusId == null) {
+            throw new InvalidOrderException("MEALS OR MENUS ARE REQUIRED");
         }
 
-        for (Integer menuId : menusId) {
-            if (menuId == null || menuId < 0) {
-                throw new InvalidMenuInformationException("INVALID MENU ID");
+        if (this.mealsId != null) {
+            for (Integer mealId : mealsId) {
+                if (mealId == null || mealId < 0) {
+                    throw new InvalidMealInformationException("INVALID MEAL ID");
+                }
+            }
+        }
+        if (this.menusId != null) {
+            for (Integer menuId : menusId) {
+                if (menuId == null || menuId < 0) {
+                    throw new InvalidMenuInformationException("INVALID MENU ID");
+                }
             }
         }
 
     }
-
 
     public Integer getStudentId() {
         return studentId;
