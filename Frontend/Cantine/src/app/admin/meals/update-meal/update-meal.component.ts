8187d3514ac689133b89ce150e7e9a07c7290b50
yahiaouihlm <yahiaouihlm@gmail.com>
2023-06-27 23:01:47 +0200

    refactor  Meal  Component

--- Frontend/Cantine/src/app/admin/meals/update-meal/update-meal.component.ts
+++ Frontend/Cantine/src/app/admin/meals/update-meal/update-meal.component.ts
@@ -9,25 +9,25 @@ import {ValidatorDialogComponent} from "../../../sharedmodule/dialogs/validator-
 import {SuccessfulDialogComponent} from "../../../sharedmodule/dialogs/successful-dialog/successful-dialog.component";
 
 @Component({
-  selector: 'app-update-meal',
-  templateUrl: './update-meal.component.html',
-  styleUrls: ["../../../../assets/styles/new-meal.component.scss"],
-  providers: [MealServiceService]
+    selector: 'app-update-meal',
+    templateUrl: './update-meal.component.html',
+    styleUrls: ["../../../../assets/styles/new-meal.component.scss"],
+    providers: [MealServiceService]
 })
-export class UpdateMealComponent  implements OnInit{
+export class UpdateMealComponent implements OnInit {
 
     private MEAL_UPDATED_SUCCESSFULLY = "Le plat a été modifié avec succès !";
-    private  MEAL_DELETED_SUCCESSFULLY = "Le plat a été supprimé avec succès !";
-    private  ERROR_OCCURRED_WHILE_UPDATING_MEAL = "Une erreur est survenue lors de la modification du plat !";
+    private MEAL_DELETED_SUCCESSFULLY = "Le plat a été supprimé avec succès !";
+    private ERROR_OCCURRED_WHILE_UPDATING_MEAL = "Une erreur est survenue lors de la modification du plat !";
     private WOULD_YOU_LIKE_TO_UPDATE_THIS_MEAL = "Voulez-vous vraiment Modifier  ce plat ?";
 
-    private  WOULD_YOU_LIKE_TO_DELETE_THIS_MEAL = "Voulez-vous vraiment supprimer ce plat ?";
-    private  ATTENTION_MEAL_PRICE= "Attention  vous  avez  saisi  un  prix  supérieur  à  50€  pour un  plat  !";
+    private WOULD_YOU_LIKE_TO_DELETE_THIS_MEAL = "Voulez-vous vraiment supprimer ce plat ?";
+    private ATTENTION_MEAL_PRICE = "Attention  vous  avez  saisi  un  prix  supérieur  à  50€  pour un  plat  !";
 
 
-    meal : Meal = new Meal();
-    submitted!  :  boolean ;
-    image! : File
+    meal: Meal = new Meal();
+    submitted!: boolean;
+    image!: File
     updatedMeal: FormGroup = new FormGroup({
         label: new FormControl('', [Validators.required, Validators.maxLength(60), Validators.minLength(3)]),
         description: new FormControl('', [Validators.required, Validators.maxLength(1600), Validators.minLength(5)]),
@@ -38,21 +38,23 @@ export class UpdateMealComponent  implements OnInit{
         status: new FormControl('', [Validators.required])
     });
 
-  constructor( private mealServiceService : MealServiceService , private  router  :  Router , private route: ActivatedRoute,private matDialog: MatDialog  ) { }
-
-  ngOnInit(): void {
-    const param = this.route.snapshot.paramMap.get('id');
-    if (param) {
-        const id = +param;
-        this.mealServiceService.getMealById(id).subscribe(data => {
-            this.meal = data;
-            this.matchFormsValue()
-            console.log(this.meal.category)
-        });
-
+    constructor(private mealServiceService: MealServiceService, private router: Router, private route: ActivatedRoute, private matDialog: MatDialog) {
     }
 
-  }
+    ngOnInit(): void {
+        const param = this.route.snapshot.paramMap.get('id');
+
+        if (param) {
+            const id = +param;
+            this.mealServiceService.getMealById(id).subscribe(data => {
+                this.meal = data;
+                this.matchFormsValue()
+                console.log(this.meal.category)
+            });
+
+        }
+
+    }
 
 
     onSubmit() {
@@ -83,47 +85,43 @@ export class UpdateMealComponent  implements OnInit{
     }
 
 
+    editMeal(): void {
+        const formData = new FormData();
+        formData.append('id', this.meal.id.toString());
+        formData.append('label', this.updatedMeal.controls['label'].value);
+        formData.append('description', this.updatedMeal.controls['description'].value);
+        formData.append('category', this.updatedMeal.controls['category'].value);
+        formData.append('price', this.updatedMeal.controls['price'].value);
+        formData.append('quantity', this.updatedMeal.controls['quantity'].value);
 
+        if (this.image != null || this.image != undefined) // envoyer  une image  uniquement si  y'a eu  une image  !
+            formData.append('image', this.image);
 
+        if (this.updatedMeal.controls['status'].value == "available") {
+            formData.append('status', "1");
+        } else {
+            formData.append('status', "0");
+        }
 
-  editMeal()  :  void {
-      const formData = new FormData();
-      formData.append('id', this.meal.id.toString());
-      formData.append('label', this.updatedMeal.controls['label'].value);
-      formData.append('description', this.updatedMeal.controls['description'].value);
-      formData.append('category', this.updatedMeal.controls['category'].value);
-      formData.append('price', this.updatedMeal.controls['price'].value);
-      formData.append('quantity', this.updatedMeal.controls['quantity'].value);
+        this.mealServiceService.editMeal(formData).subscribe((data) => {
+            if (data.message == "MEAL UPDATED SUCCESSFULLY") {
 
-      if (this.image != null || this.image != undefined) // envoyer  une image  uniquement si  y'a eu  une image  !
-          formData.append('image', this.image);
+                const result = this.matDialog.open(SuccessfulDialogComponent, {
+                    data: {message: this.MEAL_UPDATED_SUCCESSFULLY},
+                    width: '40%',
+                });
+                result.afterClosed().subscribe((result) => {
+                    this.router.navigate(['/admin/meals'], {queryParams: {reload: 'true'}})
+                });
 
-      if  (this.updatedMeal.controls['status'].value == "available") {
-          formData.append('status', "1");
-      } else {
-          formData.append('status', "0");
-      }
+            } else {
+                alert(this.ERROR_OCCURRED_WHILE_UPDATING_MEAL);
+                /*TODO    Remove  Token   */
+            }
 
-      this.mealServiceService.editMeal(formData).subscribe((data) => {
-          if  (data.message  == "MEAL UPDATED SUCCESSFULLY") {
+        });
 
-              const result = this.matDialog.open(SuccessfulDialogComponent, {
-                  data: {message: this.MEAL_UPDATED_SUCCESSFULLY},
-                  width: '40%',
-              });
-              result.afterClosed().subscribe((result) => {
-                  this.router.navigate(['/admin/meals'] ,  { queryParams: { reload: 'true' } })
-              });
-
-          }
-          else   {
-              alert(this.ERROR_OCCURRED_WHILE_UPDATING_MEAL) ;
-              /*TODO    Remove  Token   */
-          }
-
-      });
-
-  }
+    }
 
 
     delete(): void {
@@ -144,49 +142,52 @@ export class UpdateMealComponent  implements OnInit{
     }
 
 
-
-    removeMealSendReq() : void {
+    removeMealSendReq(): void {
         this.mealServiceService.deleteMeal(this.meal.id).subscribe((data) => {
-            if  (data.message  == "MEAL DELETED SUCCESSFULLY" ) {
+            if (data.message == "MEAL DELETED SUCCESSFULLY") {
 
                 const result = this.matDialog.open(SuccessfulDialogComponent, {
-                    data: {message:  this.MEAL_DELETED_SUCCESSFULLY},
+                    data: {message: this.MEAL_DELETED_SUCCESSFULLY},
                     width: '40%',
                 });
                 result.afterClosed().subscribe((result) => {
-                    this.router.navigate(['/admin/meals'] ,  { queryParams: { reload: 'true' } })
+                    this.router.navigate(['/admin/meals'], {queryParams: {reload: 'true'}})
                 });
 
-            }
-            else   {
-                alert(this.ERROR_OCCURRED_WHILE_UPDATING_MEAL) ;
+            } else {
+                alert(this.ERROR_OCCURRED_WHILE_UPDATING_MEAL);
                 /*TODO    Remove  Token   */
             }
 
-        }   );
+        });
 
 
     }
 
 
-
-
     matchFormsValue() {
-        function  getStatusFromNumber ( pstatus : number ) : string {
-            if (pstatus == 1) { return "available" } else { return "unavailable" }
+        function getStatusFromNumber(pstatus: number): string {
+            if (pstatus == 1) {
+                return "available"
+            } else {
+                return "unavailable"
+            }
         }
+
         this.updatedMeal.patchValue({
             label: this.meal.label,
             description: this.meal.description,
             price: this.meal.price,
             quantity: this.meal.quantity,
-            category :  this.meal.category,
-            status :getStatusFromNumber (this.meal.status)
+            category: this.meal.category,
+            status: getStatusFromNumber(this.meal.status)
         });
     }
+
     get f(): { [key: string]: AbstractControl } {
         return this.updatedMeal.controls;
     }
+
     onChange = ($event: Event) => {
         const target = $event.target as HTMLInputElement;
         const file: File = (target.files as FileList)[0]
@@ -194,10 +195,9 @@ export class UpdateMealComponent  implements OnInit{
     }
 
 
-    goback() : void {
-        this.router.navigate(['/admin/meals'] ) ;
+    goback(): void {
+        this.router.navigate(['/admin/meals']);
     }
-
 
 
 }
