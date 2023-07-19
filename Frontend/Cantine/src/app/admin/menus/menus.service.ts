d36aba8bf42f61143afaa8eece48105d2b2eda77
yahiaouihlm <yahiaouihlm@gmail.com>
2023-07-19 22:45:11 +0200

    add  addmenu  function  to menu   service

--- Frontend/Cantine/src/app/admin/menus/menus.service.ts
+++ Frontend/Cantine/src/app/admin/menus/menus.service.ts
@@ -1,10 +1,76 @@
 import { Injectable } from '@angular/core';
+import {Menu} from "../../sharedmodule/models/menu";
+import {HttpClient, HttpErrorResponse, HttpStatusCode} from "@angular/common/http";
+import {catchError, throwError} from "rxjs";
+import {ErrorResponse} from "../../sharedmodule/models/ErrorResponse";
+import {MatDialog} from "@angular/material/dialog";
+import {Router} from "@angular/router";
+import {ExceptionDialogComponent} from "../../sharedmodule/dialogs/exception-dialog/exception-dialog.component";
 
 @Injectable()
 export class MenusService {
 
-  constructor() { }
+  private BASIC_ENDPOINT = "http://localhost:8080/cantine/" + 'api/admin/meals';
+  private  ADD_MENU_URL = this.BASIC_ENDPOINT  + '/add';
+  constructor(private httpClient: HttpClient , private matDialog: MatDialog , private  router : Router ) { }
 
 
+
+  sendMenu (menu :  Menu ){
+    return this.httpClient.post <Menu>(this.ADD_MENU_URL, menu).pipe(
+        catchError( (error) => this.handleError(error))
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    const errorObject = error.error as ErrorResponse;
+    let  errorMessage = errorObject.exceptionMessage;
+
+    if  (error.status == HttpStatusCode.BadRequest || error.status == HttpStatusCode.NotAcceptable){
+      errorMessage = "Veuillez  vérifier  les  données  saisies  !";
+      this.openDialog(errorMessage,  error.status);
+
+    }
+    /*else if (error.status == HttpStatusCode.Conflict) {
+      this.openDialog(errorMessage, error.status);
+    }
+    else if  (error.status == HttpStatusCode.InternalServerError){
+      errorMessage =  "Une  erreur  interne  est  survenue  !"
+      this.openDialog(errorMessage, error.status);
+    }
+    else if  (error.status == HttpStatusCode.NotFound){
+      errorMessage =  "Ce  plat  n'existe  pas  ! \n  il ce peut qu'il a été supprimé  !"
+      this.openDialog(errorMessage, error.status);
+    }
+    else {
+      errorMessage =  "Une  erreur  est  survenue  !"
+      console.log(error.status)
+      this.openDialog(errorMessage, error.status);
+    }*/
+    return throwError(() => new Error(errorMessage));
+
+  }
+
+
+
+
+  private openDialog(message: string , httpError :  HttpStatusCode ): void {
+    const result = this.matDialog.open(ExceptionDialogComponent, {
+      data: {message: message},
+      width: '40%',
+    });
+
+    result.afterClosed().subscribe((confirmed: boolean) => {
+      if (httpError == HttpStatusCode.BadRequest || httpError == HttpStatusCode.NotAcceptable || httpError== HttpStatusCode.Conflict || httpError== HttpStatusCode.NotFound){
+        this.router.navigate(['/admin/menus'] , { queryParams: { reload: 'true' } });
+      }
+      else {
+        /* TODO  remove THE  Token  */
+        //this.router.navigate(['/cantine/home'] , { queryParams: { reload: 'true' } });
+      }
+
+    });
+
+  }
 
 }
--- Frontend/Cantine/src/app/admin/menus/new-menu/new-menu.component.ts
+++ Frontend/Cantine/src/app/admin/menus/new-menu/new-menu.component.ts
@@ -9,7 +9,9 @@ import {Meal} from "../../../sharedmodule/models/meal";
   templateUrl: './new-menu.component.html',
   styleUrls: ['../../../../assets/styles/new-meal.component.scss']
 })
+
 export class NewMenuComponent {
+  private   ATTENTION_MEAL_PRICE = "Attention, Vous  avez  Saisie un  Priw de 80€  Pour un Menu  !"
   submitted = false;
   image!: File
   mealsContainMenu:  Meal[] = []
@@ -19,11 +21,26 @@ export class NewMenuComponent {
     price: new FormControl('', [Validators.required, Validators.min(0.01), Validators.max(999.99)]),
     quantity: new FormControl('', [Validators.required, Validators.min(0), Validators.max(2147483501), Validators.pattern("^[0-9]+$")]),
     image: new FormControl('', [Validators.required]),
-    status: new FormControl('', [Validators.required])
+    status: new FormControl('', [Validators.required]),
   });
 
   closedDialog   =  false;
   constructor( private matDialog: MatDialog) {}
+
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.newMenu.invalid || this.mealsContainMenu.length < 2) {
+      return;
+    }
+    if (this.newMenu.controls["price"].value > 50) {
+      alert(this.ATTENTION_MEAL_PRICE)
+    }
+
+    console.log("le  formulaire  est bien  valider  et  pret  a  etre  envoyer  au  serveur");
+
+  }
+
   onOpenListMealDialog() {
     this.closedDialog = false;
     const result = this.matDialog.open(ListMealsComponent);
