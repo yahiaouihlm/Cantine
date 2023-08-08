df9dbd9aeb6d36f204e02dd2b72a9b81a0e150b9
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-08 18:02:57 +0200

    make  sign Up  form

--- Frontend/Cantine/src/app/admin/dashbord/admin-dashboard/sign-up/sign-up.component.ts
+++ Frontend/Cantine/src/app/admin/dashbord/admin-dashboard/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,5 +8,34 @@ import { Component } from '@angular/core';
   ]
 })
 export class SignUpComponent {
+   submitted = false;
+    image!: File ;
+  isLoaded = false;
+  adminForm: FormGroup = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+  /*  label: new FormControl('', [Validators.required, Validators.maxLength(60), Validators.minLength(3)]),
+    description: new FormControl('', [Validators.required, Validators.maxLength(1700), Validators.minLength(5)]),
+    price: new FormControl('', [Validators.required, Validators.min(0.01), Validators.max(999.99)]),
+    quantity: new FormControl('', [Validators.required, Validators.min(0), Validators.max(2147483501), Validators.pattern("^[0-9]+$")]),
+    image: new FormControl('', [Validators.required]),
+    status: new FormControl('', [Validators.required]),*/
+  });
+
+
+  onSubmit() {
+    this.submitted = true;
+    this.isLoaded = true;
+    if (this.adminForm.invalid) {
+      return;
+    }
+  }
+  onChange = ($event: Event) => {
+    const target = $event.target as HTMLInputElement;
+    const file: File = (target.files as FileList)[0]
+    this.image = file;
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.adminForm.controls;
+  }
 
 }
