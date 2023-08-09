197bdd12f2c9cb12b78e41589bf2f7afb3e1c9be
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-09 17:30:04 +0200

    add  authentication  component  to  code  cantine

---
+++ Frontend/Cantine/src/app/core-cantine/authentication/authentication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authentication',
+  templateUrl: './authentication.component.html',
+  styles: [
+  ]
+})
+export class AuthenticationComponent {
+
+}
--- Frontend/Cantine/src/app/core-cantine/core-cantine.module.ts
+++ Frontend/Cantine/src/app/core-cantine/core-cantine.module.ts
@@ -11,6 +11,7 @@ import {MatListModule} from "@angular/material/list";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import { AuthenticationComponent } from './authentication/authentication.component';
 
 
 
@@ -38,6 +39,7 @@ const routes: Routes = [
     MainCoreCantineComponent,
     MealsComponent,
     MenuComponent,
+    AuthenticationComponent,
   ],
   imports: [
     CommonModule,
