795088f7efbe7a9e28694c33b27a81c20a980224
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-25 23:11:19 +0200

    add  Login Authentication Object

--- Frontend/Cantine/src/app/core-cantine/core-cantine.service.ts
+++ Frontend/Cantine/src/app/core-cantine/core-cantine.service.ts
@@ -6,10 +6,15 @@ import {Menu} from "../sharedmodule/models/menu";
 @Injectable()
 export class CoreCantineService {
   private  BASIC_MEAL_URL = "http://localhost:8080/cantine/api/" ;
+  private  USER_AUTHENTICATION_ENDPOINT =  "http://localhost:8080/"+ 'login';
   private  GET_ALL_MEALS_ENDPOINT = this.BASIC_MEAL_URL +  'meals/getAll'
-
   private GET_ALL_MENUS_ENDPOINT =  this.BASIC_MEAL_URL + 'menus/getAll';
   constructor(private httpClient: HttpClient) { }
+
+
+    userAuthentication(obj: any) {
+        return this.httpClient.post(this.USER_AUTHENTICATION_ENDPOINT, {email, password});
+    }
 
    getAllMeals () {
     return this.httpClient.get<Meal[]>(this.GET_ALL_MEALS_ENDPOINT)
