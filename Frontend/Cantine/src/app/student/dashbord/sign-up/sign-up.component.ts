a37bb3d82972c56d28e0c458f0a496d36a5a0b60
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-29 15:14:03 +0200

    add  sign Up  compnent  for  student

--- Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
+++ Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
@@ -1,42 +1,55 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import Validation from "../../../sharedmodule/functions/validation";
 import {Observable, of} from "rxjs";
 import {Adminfunction} from "../../../sharedmodule/models/adminfunction";
 import {StudentClass} from "../../../sharedmodule/models/studentClass";
+import {StudentDashboardService} from "../student-dashboard.service";
+import {MatDialog} from "@angular/material/dialog";
+import {SharedService} from "../../../sharedmodule/shared.service";
 
 @Component({
-    selector: 'app-sign-in',
-    templateUrl: './sign-up.component.html',
-    styles: []
+  selector: 'app-sign-in',
+  templateUrl: './sign-up.component.html',
+  styles: [
+  ] , providers: [StudentDashboardService]
 })
-export class SignUpComponent {
+export class SignUpComponent  implements   OnInit {
 
-    image: File | undefined;
+    image!: File;
     submitted = false;
     existEmail = false;
-    studentClass: Observable<StudentClass[]> = of([]);
-    studentForm: FormGroup = new FormGroup({
-            firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
-            lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
-            email: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.pattern(Validation.EMAIL_REGEX)]),
-            password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
-            confirmPassword: new FormControl("", [Validators.required]),
-            birthDate: new FormControl('', [Validators.required]),
-            phoneNumber: new FormControl('', [Validators.pattern(Validation.FRENCH_PHONE_REGEX)]),
-            town: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.minLength(3)]),
-            studentClass: new FormControl('', [Validators.required]),
-            image: new FormControl(''),
 
-        },
-        {
-            validators: [Validation.match('password', 'confirmPassword')]
-        }
-    );
+   studentClass$: Observable<StudentClass[]> = of([]);
+  studentForm: FormGroup = new FormGroup({
+        firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+        lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+        email: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.pattern(Validation.EMAIL_REGEX)]),
+        password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
+        confirmPassword: new FormControl("", [Validators.required]),
+        birthDate: new FormControl('', [Validators.required]),
+        phoneNumber: new FormControl('', [Validators.pattern(Validation.FRENCH_PHONE_REGEX)]),
+        town: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.minLength(3)]),
+        studentClass: new FormControl('', [Validators.required]),
+        image: new FormControl(''),
+
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+  );
+
+
+  constructor( private  studentService: StudentDashboardService, private matDialog: MatDialog , private sharedService: SharedService) {
+  }
+
+    ngOnInit(): void {
+       this.studentClass$ =  this.studentService.getAllStudentClass();
+    }
+
 
 
     onSubmit() {
-
 
     }
 
--- Frontend/Cantine/src/app/student/dashbord/student-dashboard.service.ts
+++ Frontend/Cantine/src/app/student/dashbord/student-dashboard.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatDialog} from "@angular/material/dialog";
+import {Router} from "@angular/router";
+import {Adminfunction} from "../../sharedmodule/models/adminfunction";
+import {StudentClass} from "../../sharedmodule/models/studentClass";
 
 @Injectable()
 export class StudentDashboardService {
 
-  constructor() { }
+  private  BASIC_ENDPOINT = "http://localhost:8080/cantine/student/";
+
+  private  GET_ALL_STUDENT_CLASS = this.BASIC_ENDPOINT + 'getAllStudentClass';
+  constructor(private httpClient: HttpClient, private matDialog: MatDialog, private router: Router) { }
+
+
+  getAllStudentClass () {
+    return this.httpClient.get<StudentClass[]>(this.GET_ALL_STUDENT_CLASS);
+  }
 }
