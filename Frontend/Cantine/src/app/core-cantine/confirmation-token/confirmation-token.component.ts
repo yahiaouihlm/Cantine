4c84cb11efdf90a921c1f934a30527ac22ba5d7a
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-29 13:22:10 +0200

    check token  validity  secessfully

--- Frontend/Cantine/src/app/core-cantine/confirmation-token/confirmation-token.component.ts
+++ Frontend/Cantine/src/app/core-cantine/confirmation-token/confirmation-token.component.ts
@@ -1,24 +1,60 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {AdminService} from "../../admin/dashbord/admin.service";
+import {normalizeExtraEntryPoints} from "@angular-devkit/build-angular/src/webpack/utils/helpers";
+import {HttpStatusCode} from "@angular/common/http";
 
 @Component({
   selector: 'app-confirmation-token',
   templateUrl: './confirmation-token.component.html',
-  styles: [
-  ]
+  styles: [] ,
+  providers : [AdminService ]
 })
-export class ConfirmationTokenComponent  implements  OnInit{
+export class ConfirmationTokenComponent  implements  OnInit {
 
 
+  serverResponse: string = "";
+  activated: boolean = false;
 
-  constructor(private   route :  ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private adminService: AdminService) {
+  }
+
   ngOnInit(): void {
     const token = this.route.snapshot.paramMap.get('token');
     if (token) {
-     /* const token = +token;*/
-        console.log(token)
+      this.checkTokenValidityAdmin(token);
     }
 
   }
 
+
+  checkTokenValidityAdmin(token: string) {
+    this.adminService.checkTokenValidityAdmin(token).subscribe({
+      next: (response) => {
+        if (response.message == "TOKEN VALID") {
+          this.serverResponse = "Votre compte a été activé avec succès";
+          this.activated = true;
+        }
+
+      },
+      error: (error) => {
+        if (error.status === HttpStatusCode.Unauthorized) {
+          this.serverResponse = "Votre  Token est Expiré ";
+        } else if ((error.status === HttpStatusCode.NotFound) || (error.status === HttpStatusCode.BadRequest)) {
+          this.serverResponse = "Votre  Token est Invalide ";
+        } else {
+          console.log(error.status)
+          this.serverResponse = "Une erreur s'est produite pendant la vérification du token";
+        }
+
+      }
+
+    })
+
+
+  }
 }
+
+
+
+
