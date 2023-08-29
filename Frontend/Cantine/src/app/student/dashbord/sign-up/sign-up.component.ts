e067a24de20ede3b44df50a368d8035378b3ca10
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-29 14:48:43 +0200

    add  student  dashbord  service

--- Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
+++ Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
@@ -1,46 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import Validation from "../../../sharedmodule/functions/validation";
 import {Observable, of} from "rxjs";
 import {Adminfunction} from "../../../sharedmodule/models/adminfunction";
+import {StudentClass} from "../../../sharedmodule/models/studentClass";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-up.component.html',
-  styles: [
-  ]
+    selector: 'app-sign-in',
+    templateUrl: './sign-up.component.html',
+    styles: []
 })
 export class SignUpComponent {
 
-    image :  File | undefined;
+    image: File | undefined;
     submitted = false;
     existEmail = false;
-    studentClass: Observable<Adminfunction[]> = of([]);
-  studentForm: FormGroup = new FormGroup({
-        firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
-        lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
-        email: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.pattern(Validation.EMAIL_REGEX)]),
-        password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
-        confirmPassword: new FormControl("", [Validators.required]),
-        birthDate: new FormControl('', [Validators.required]),
-        phoneNumber: new FormControl('', [Validators.pattern(Validation.FRENCH_PHONE_REGEX)]),
-        town: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.minLength(3)]),
-        studentClass: new FormControl('', [Validators.required]),
-        image: new FormControl(''),
+    studentClass: Observable<StudentClass[]> = of([]);
+    studentForm: FormGroup = new FormGroup({
+            firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+            lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+            email: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.pattern(Validation.EMAIL_REGEX)]),
+            password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
+            confirmPassword: new FormControl("", [Validators.required]),
+            birthDate: new FormControl('', [Validators.required]),
+            phoneNumber: new FormControl('', [Validators.pattern(Validation.FRENCH_PHONE_REGEX)]),
+            town: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.minLength(3)]),
+            studentClass: new FormControl('', [Validators.required]),
+            image: new FormControl(''),
 
-      },
-      {
-        validators: [Validation.match('password', 'confirmPassword')]
-      }
-  );
-
-
-    onSubmit()     {
+        },
+        {
+            validators: [Validation.match('password', 'confirmPassword')]
+        }
+    );
 
 
-  }
+    onSubmit() {
 
 
+    }
 
 
     onChange = ($event: Event) => {
@@ -52,7 +50,6 @@ export class SignUpComponent {
     get f(): { [key: string]: AbstractControl } {
         return this.studentForm.controls;
     }
-
 
 
 }
