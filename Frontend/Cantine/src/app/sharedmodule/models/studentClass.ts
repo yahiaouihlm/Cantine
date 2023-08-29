8ca22d4d9d2e6e3a9c08e90db8ceb27bd923feee
yahiaouihlm <yahiaouihlm@gmail.com>
2023-08-29 14:25:42 +0200

    add studentClass  model

---
+++ Frontend/Cantine/src/app/sharedmodule/models/studentClass.ts
@@ -0,0 +1,5 @@
+export   class  StudentClass {
+
+    id: number | undefined;
+    name!: string
+}
--- Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
+++ Frontend/Cantine/src/app/student/dashbord/sign-up/sign-up.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
+import Validation from "../../../sharedmodule/functions/validation";
+import {Observable, of} from "rxjs";
+import {Adminfunction} from "../../../sharedmodule/models/adminfunction";
 
 @Component({
   selector: 'app-sign-in',
@@ -7,5 +11,48 @@ import { Component } from '@angular/core';
   ]
 })
 export class SignUpComponent {
+
+    image :  File | undefined;
+    submitted = false;
+    existEmail = false;
+    studentClass: Observable<Adminfunction[]> = of([]);
+  studentForm: FormGroup = new FormGroup({
+        firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+        lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+        email: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.pattern(Validation.EMAIL_REGEX)]),
+        password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(6)]),
+        confirmPassword: new FormControl("", [Validators.required]),
+        birthDate: new FormControl('', [Validators.required]),
+        phoneNumber: new FormControl('', [Validators.pattern(Validation.FRENCH_PHONE_REGEX)]),
+        town: new FormControl('', [Validators.required, Validators.maxLength(1000), Validators.minLength(3)]),
+        studentClass: new FormControl('', [Validators.required]),
+        image: new FormControl(''),
+
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')]
+      }
+  );
+
+
+    onSubmit()     {
+
+
+  }
+
+
+
+
+    onChange = ($event: Event) => {
+        const target = $event.target as HTMLInputElement;
+        const file: File = (target.files as FileList)[0]
+        this.image = file;
+    }
+
+    get f(): { [key: string]: AbstractControl } {
+        return this.studentForm.controls;
+    }
+
+
 
 }
