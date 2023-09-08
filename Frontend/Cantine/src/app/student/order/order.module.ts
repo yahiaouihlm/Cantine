0988b99688a94da0383a99e6192b484f8b89017a
yahiaouihlm <yahiaouihlm@gmail.com>
2023-09-08 18:40:20 +0200

    add  order module  with  order-dashbord component

---
+++ Frontend/Cantine/src/app/student/order/order.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OrderDashbordComponent } from './order-dashbord/order-dashbord.component';
+
+
+
+@NgModule({
+  declarations: [
+    OrderDashbordComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class OrderModule { }
