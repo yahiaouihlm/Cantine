86077c38a986ba878f2318645203ff0999bec20c
yahiaouihlm <yahiaouihlm@gmail.com>
2023-09-10 15:23:28 +0200

    make the  function  to  check   student connectivity  and  redirect  to  signIn component

--- Frontend/Cantine/src/app/sharedmodule/functions/malfunctions.ts
+++ Frontend/Cantine/src/app/sharedmodule/functions/malfunctions.ts
@@ -1,9 +1,38 @@
 import {AuthObject} from "../models/authObject";
 import {Router} from "@angular/router";
+import {SharedService} from "../shared.service";
 
 export  default class Malfunctions {
 
 
+    public static checkStudentConnectivity(router: Router ,  sharedService : SharedService)  {
+        let  interdiction  = () => {
+            localStorage.clear();
+            router.navigate(['cantine/signIn']).then(r =>
+                window.location.reload());
+        }
+
+        let authObj = localStorage.getItem('authObject');
+        if (!authObj) {
+            interdiction();
+            return
+        }
+        else  {
+            let authObject = JSON.parse(authObj) as AuthObject;
+            let  studentId = authObject.id
+           let result =   sharedService.getStudentById(studentId).subscribe({
+                next: (response) => {
+                    return true;
+                },
+                error: (error) => {
+                    interdiction();
+                }
+            });
+
+        }
+
+
+    }
     public  static getTokenFromLocalStorage() :  string {
         let authObj = localStorage.getItem('authObject')
         if (!authObj) {
