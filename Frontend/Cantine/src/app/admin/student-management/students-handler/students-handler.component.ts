767009a6f493cd90dfba6569985baf4d8812626a
yahiaouihlm <yahiaouihlm@gmail.com>
2023-10-03 17:28:34 +0200

    make  html template  for  the  student search

--- Frontend/Cantine/src/app/admin/student-management/students-handler/students-handler.component.ts
+++ Frontend/Cantine/src/app/admin/student-management/students-handler/students-handler.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-students-handler',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class StudentsHandlerComponent {
+
+  submitted =  false ;
+  studentSeeked: FormGroup = new FormGroup({
+    firstName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+    lastName: new FormControl('', [Validators.required, Validators.maxLength(90), Validators.minLength(3)]),
+    birthdate: new FormControl('', [Validators.required]),
+  });
+
+
+
+
+
+
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.studentSeeked.controls;
+  }
 
 }
