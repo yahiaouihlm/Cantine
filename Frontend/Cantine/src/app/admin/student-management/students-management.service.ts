d939ef32ed6fe6023b21c06711d39a6a4206a9c8
yahiaouihlm <yahiaouihlm@gmail.com>
2023-10-04 22:47:52 +0200

    add getStudentById

--- Frontend/Cantine/src/app/admin/student-management/students-management.service.ts
+++ Frontend/Cantine/src/app/admin/student-management/students-management.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpStatusCode} from "@angular/common/http";
 import Malfunctions from "../../sharedmodule/functions/malfunctions";
 import {User} from "../../sharedmodule/models/user";
+import {ErrorResponse} from "../../sharedmodule/models/ErrorResponse";
+import {DialogErrors} from "../../sharedmodule/functions/dialogueErrors";
+import {catchError, throwError} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
 
 @Injectable()
 export class StudentsManagementService {
 
   private  BASIC_ADMIN_URL =  "http://localhost:8080/cantine/admin/adminDashboard/works";
-  private  GET_STUDENTS =  this.BASIC_ADMIN_URL + "/GetStudents"
-  constructor(private   httpClient:HttpClient) { }
+  private  GET_STUDENTS =  this.BASIC_ADMIN_URL + "/getStudent"
+
+  private GET_STUDENT_BY_ID = this.BASIC_ADMIN_URL + "/GetStudentById";
+  constructor(private   httpClient:HttpClient , private matDialog :  MatDialog) { }
 
 
 
+  getStudentById(id : string) {
+    let token  =  Malfunctions.getTokenFromLocalStorage();
+    const headers = new HttpHeaders().set('Authorization', token);
+    const params =  new  HttpParams().set('studentId' , id.toString())
+    return this.httpClient.get<User>( this.GET_STUDENT_BY_ID, { headers : headers , params : params }).pipe(
+        catchError((error) => this.handleError(error))
+    );
+  }
   getStudents( user  :  User ) {
     let token  =  Malfunctions.getTokenFromLocalStorage();
     const headers = new HttpHeaders().set('Authorization', token);
@@ -20,4 +34,20 @@ export class StudentsManagementService {
                                      .set('birthdateAsString', user.birthdate.toString())
     return this.httpClient.get<User[]>(this.GET_STUDENTS , { params : params , headers : headers });
   }
+
+
+  private handleError(error: HttpErrorResponse) {
+    const errorObject = error.error as ErrorResponse;
+    let errorMessage = errorObject.exceptionMessage;
+
+    if (error.status == HttpStatusCode.InternalServerError) {
+      errorMessage = "Une  erreur    est  survenue  !"
+      new DialogErrors(this.matDialog).openDialog(errorMessage, error.status);
+    } else {
+      new DialogErrors(this.matDialog).openDialog(errorMessage, error.status);
+    }
+    return throwError(() => new Error(error.error));
+
+  }
+
 }
