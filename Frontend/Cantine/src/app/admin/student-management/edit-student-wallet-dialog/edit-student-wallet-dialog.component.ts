7e0c57d71aeb7d83625c34aa745a716b8a6a8a03
yahiaouihlm <yahiaouihlm@gmail.com>
2023-10-06 19:36:16 +0200

    update edit-student-wallet-dialog.component.ts

--- Frontend/Cantine/src/app/admin/student-management/edit-student-wallet-dialog/edit-student-wallet-dialog.component.ts
+++ Frontend/Cantine/src/app/admin/student-management/edit-student-wallet-dialog/edit-student-wallet-dialog.component.ts
@@ -5,38 +5,36 @@ import Validation from "../../../sharedmodule/functions/validation";
 import {StudentsManagementService} from "../students-management.service";
 
 @Component({
-    selector: 'app-editer-salaire-etudiant-dialog',
-    templateUrl: `./edit-student-wallet-dialog.component.html`,
-    styles: [],
-    providers: [StudentsManagementService]
+  selector: 'app-editer-salaire-etudiant-dialog',
+  templateUrl: `./edit-student-wallet-dialog.component.html`,
+  styles: [] ,
+  providers: [StudentsManagementService]
 })
 export class EditStudentWalletDialogComponent {
 
-    submitted: boolean = false;
-    isLoaded: boolean = false;
-    amountForm: FormGroup = new FormGroup({
-        amount: new FormControl('', [Validators.required, Validators.max(200), Validators.min(10)]),
+  submitted: boolean = false;
+  isLoaded: boolean = false;
+  amountForm: FormGroup = new FormGroup({
+    amount: new FormControl('', [Validators.required ,   Validators.max(200)  ,  Validators.min(10)]),
+  });
+
+   constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string ,  userid : number} ,  private dialogRef: MatDialogRef<EditStudentWalletDialogComponent> ,  private studentsManagementService :  StudentsManagementService  ) { }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.amountForm.invalid) {
+      return;
+    }
+    this.isLoaded =  true ;
+    this.studentsManagementService.sendStudentWallet(this.data.userid , this.amountForm.value.amount).subscribe({
+        next: (response) => { this.isLoaded = false} ,
+        error : (error) => { this.isLoaded=  false } ,
     });
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.amountForm.controls;
+  }
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string, userid: number }, private dialogRef: MatDialogRef<EditStudentWalletDialogComponent>, private studentsManagementService: StudentsManagementService) {
-    }
-
-    onSubmit(): void {
-        this.submitted = true;
-        if (this.amountForm.invalid) {
-            return;
-        }
-        this.isLoaded = true;
-        this.studentsManagementService.sendStudentWallet(15, this.amountForm.value.amount).subscribe(data => {
-            this.isLoaded = false;
-        }, error => {
-            this.isLoaded = false;
-        });
-    }
-
-    get f(): { [key: string]: AbstractControl } {
-        return this.amountForm.controls;
-    }
 
 
 }
