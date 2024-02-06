b30a3314ecdefc0fbdf85b95407a9971f9d21176
hayahiaoui <hayahiaoui@sqli.com>
2024-02-06 23:14:49 +0100

    add  field  to store the  glabal variables

---
+++ Frontend/Cantine/src/app/sharedmodule/constants/IConstantsMessages.ts
@@ -0,0 +1,8 @@
+export module IConstantsMessages {
+    export let ADMIN_ADDED_SUCCESSFULLY = "ADMIN ADDED SUCCESSFULLY";
+    export  const USER_DISABLED_ACCOUNT : string  = "DISABLED ACCOUNT";
+    export const USER_WRONG_CREDENTIALS = "WRONG CREDENTIALS";
+    export const ADMIN_INVALID_ACCOUNT = "INVALID ACCOUNT";
+    export const  ADMIN_ROLE = "ROLE_ADMIN";
+    export  const STUDENT_ROLE = "ROLE_STUDENT"
+}
---
+++ Frontend/Cantine/src/app/sharedmodule/constants/IConstantsURL.ts
@@ -0,0 +1,6 @@
+export module IConstantsURL {
+    export const ADMIN_HOME_URL = "cantine/admin";
+    export const STUDENT_HOME_URL = "cantine/home";
+    export const  FORGOT_PASSWORD_URL = "cantine/user/forgot-password";
+    export const SIGN_IN_URL = "cantine/signIn";
+}
