918fd2ff924aca19b2b46bf8336cedd70d74c2d2
hayahiaoui <hayahiaoui@sqli.com>
2024-02-29 20:58:33 +0100

    change  delete  Menu  endpoint

--- Backend/Cantine/src/main/java/fr/sqli/cantine/controller/food/IMenuController.java
+++ Backend/Cantine/src/main/java/fr/sqli/cantine/controller/food/IMenuController.java
@@ -38,7 +38,7 @@ public interface IMenuController {
     @PostMapping(value = ENDPOINT_UPDATE_MENU_URL,  consumes = MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ResponseDtout> update (MenuDtoIn menuDtoIn) throws InvalidFormatImageException, InvalidImageException, ImagePathException, IOException, InvalidFoodInformationException, ExistingFoodException, FoodNotFoundException, UnavailableFoodException;
 
-    @DeleteMapping(value = ENDPOINT_DELETE_MENU_URL)
+    @PostMapping(value = ENDPOINT_DELETE_MENU_URL)
     public ResponseEntity<ResponseDtout> deleteMenu(@RequestParam("uuidMenu")String uuidMenu) throws ImagePathException, InvalidFoodInformationException, FoodNotFoundException, RemoveFoodException;
 
     @PostMapping(value = ENDPOINT_ADD_MENU_URL , consumes = MULTIPART_FORM_DATA_VALUE )
