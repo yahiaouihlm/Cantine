e27cf3b7ad287a6f3aa19bdea6eb927d9db92c22
hayahiaoui <hayahiaoui@sqli.com>
2024-03-14 14:28:15 +0100

    update  student-transaction-history/student-transaction-history.component.

--- Frontend/Cantine/src/app/admin/student-management/student-transaction-history/student-transaction-history.component.ts
+++ Frontend/Cantine/src/app/admin/student-management/student-transaction-history/student-transaction-history.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Malfunctions from "../../../sharedmodule/functions/malfunctions";
+import {ActivatedRoute, Router} from "@angular/router";
+import {IConstantsURL} from "../../../sharedmodule/constants/IConstantsURL";
+import {StudentsManagementService} from "../students-management.service";
+import {User} from "../../../sharedmodule/models/user";
+import {Observable, of} from "rxjs";
+import {Transaction} from "../../../sharedmodule/models/Transaction";
 
 @Component({
   selector: 'app-student-transaction-history',
   templateUrl: './student-transaction-history.component.html',
-  styles: [
-  ]
+  styles: [],
+    providers: [StudentsManagementService]
 })
-export class StudentTransactionHistoryComponent {
+export class StudentTransactionHistoryComponent  implements  OnInit{
+
+  transactions$: Observable<Transaction[]> = of([]);
+
+  constructor(private router: Router , private route : ActivatedRoute ,private studentsManagementService :StudentsManagementService ) {}
+
+  ngOnInit(): void {
+    if (!Malfunctions.checkAdminConnectivityAndMakeRedirection(this.router)) {
+      return;
+    }
+    this.route.queryParams.subscribe(param => {
+      const studentUuid = param['studentUuid'];
+      if (studentUuid == undefined) {
+        this.router.navigate([IConstantsURL.ADMIN_STUDENTS_URL]).then(r => window.location.reload());
+      }
+      this.transactions$ = this.studentsManagementService.getStudentTransactions(studentUuid);
+      console.log(this.transactions$);
+    });
+
+
+
+  }
 
 }
--- Frontend/Cantine/src/app/admin/student-management/students-management.service.ts
+++ Frontend/Cantine/src/app/admin/student-management/students-management.service.ts
@@ -10,13 +10,14 @@ import {error} from "@angular/compiler-cli/src/transformers/util";
 import {NormalResponse} from "../../sharedmodule/models/NormalResponse";
 import {IConstantsURL} from "../../sharedmodule/constants/IConstantsURL";
 import {Router} from "@angular/router";
+import {Transaction} from "../../sharedmodule/models/Transaction";
 
 @Injectable()
 export class StudentsManagementService {
 
     private BASIC_ADMIN_URL = "http://localhost:8080/cantine/admin/adminDashboard/works";
-    private GET_STUDENTS = this.BASIC_ADMIN_URL + "/getStudents"
 
+    private GET_STUDENT_TRANSACTIONS_ENDPOINT = this.BASIC_ADMIN_URL + "/getStudentTransactions";
     private GET_STUDENT = this.BASIC_ADMIN_URL + "/getStudent";
     private GET_STUDENT_BY_UUID = this.BASIC_ADMIN_URL + "/getStudentByUuId";
 
@@ -28,6 +29,14 @@ export class StudentsManagementService {
     constructor(private httpClient: HttpClient, private matDialog: MatDialog,  private router: Router) {
     }
 
+    getStudentTransactions(studentUuid: string) {
+        let token = Malfunctions.getTokenFromLocalStorage();
+        const headers = new HttpHeaders().set('Authorization', token);
+        const params = new HttpParams().set('studentUuid', studentUuid);
+        return this.httpClient.get<Transaction[]>(this.GET_STUDENT_TRANSACTIONS_ENDPOINT, {headers: headers, params: params}).pipe(
+            catchError((error) => this.handleGetStudentError(error))
+        );
+    }
 
     updateStudentEmail(studentUuid: string, newEmail: string) {
         let token = Malfunctions.getTokenFromLocalStorage();
@@ -67,7 +76,6 @@ export class StudentsManagementService {
             catchError((error) => this.handleGetStudentError(error))
         );
     }
-
 
     getStudentByEmail(email: string) {
         let token = Malfunctions.getTokenFromLocalStorage();
@@ -117,20 +125,6 @@ export class StudentsManagementService {
             this.router.navigate([IConstantsURL.HOME_URL]).then(window.location.reload);
         }
         return  throwError(() => new Error(errorMessage))
-    }
-
-    private handleError(error: HttpErrorResponse) {
-        const errorObject = error.error as ErrorResponse;
-        let errorMessage = errorObject.exceptionMessage;
-
-        if (error.status == HttpStatusCode.InternalServerError) {
-            errorMessage = "Une  erreur    est  survenue  !"
-            new DialogErrors(this.matDialog).openDialog(errorMessage);
-        } else {
-            new DialogErrors(this.matDialog).openDialog(errorMessage);
-        }
-        return throwError(() => new Error(error.error));
-
     }
 
     private  handleUpdateStudentEmailError(error: HttpErrorResponse) {
@@ -189,7 +183,9 @@ export class StudentsManagementService {
         }
         else  if ( error.status ==  HttpStatusCode.NotFound) {
             errorMessage = "Etudiant non trouvé";
+            localStorage.clear();
             this.dialog.openDialog(errorMessage);
+            this.router.navigate([IConstantsURL.SIGN_IN_URL]).then(window.location.reload);
         }
         else if (error.status == HttpStatusCode.Unauthorized) {
             this.router.navigate([IConstantsURL.SIGN_IN_URL]).then(window.location.reload);
