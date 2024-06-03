1480050a7f478a694bc973fb4393f8fb2b8b4341
hayahiaoui <hayahiaoui@sqli.com>
2024-06-03 13:49:53 +0200

    add  meals  styles

--- Mobile/cantine/src/app/core-cantine/core-cantine.service.ts
+++ Mobile/cantine/src/app/core-cantine/core-cantine.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Meal} from "../shared-module/models/meals";
 
-@Injectable({
-  providedIn: 'root'
-
-})
+@Injectable()
 export class CoreCantineService {
-
+  private BASIC_URL = "http://localhost:8080/cantine/api/getAll/";
+  private GET_ALL_MEALS_ENDPOINT = this.BASIC_URL + 'meals'
 
   constructor(private httpClient: HttpClient) {
   }
 
 
-  getCategories() {
-    return this.httpClient.get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json');
+  getAllAvailableMeals() {
+    return this.httpClient.get<Meal[]>(this.GET_ALL_MEALS_ENDPOINT)
   }
 }
--- Mobile/cantine/src/app/core-cantine/meals/meals.component.ts
+++ Mobile/cantine/src/app/core-cantine/meals/meals.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {connection} from "../../shared-module/functions/connection";
+import {IConstantsURL} from "../../shared-module/constants/IConstantsURL";
+import {Router} from "@angular/router";
+import {Observable, of} from "rxjs";
+import {Meal} from "../../shared-module/models/meals";
+import {CoreCantineService} from "../core-cantine.service";
 
 @Component({
   selector: 'app-meals',
   templateUrl: './meals.component.html',
   styleUrls: ['./meals.component.scss'],
+  providers:[CoreCantineService]
 })
-export class MealsComponent  implements OnInit {
+export class MealsComponent implements OnInit {
 
-  constructor() { }
+  meals$: Observable<Meal[]> = of([]);
+  constructor(private router: Router, private coreCantineService : CoreCantineService) {
+  }
 
-  ngOnInit() {}
+
+  ngOnInit() {
+    if (!connection.checkStudentConnection()) {
+      this.router.navigate([IConstantsURL.STUDENT_SIGN_IN]).then()
+      localStorage.clear();
+    }
+    this.meals$ = this.coreCantineService.getAllAvailableMeals();
+
+  }
 
 }
